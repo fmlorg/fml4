560aaf6ee1a23e1f9c9affa2bd077b7aaa0b8ad7
fukachan <fukachan>
1994-04-08 19:54:09 +0000

    fml.pl (fml) config.ph's dir library'dir

--- C/fml.c
+++ C/fml.c
@@ -1,16 +1,15 @@
-/*
-$Header$
-$RCSfile$
-*/
+#include <stdio.h>
 
 static char rcsid[] = "$Id$";
-#include <stdio.h>
-#include <sys/types.h>
 
 main()
 {
   setuid(geteuid());
-  setgid(getegid());
-  execl("/home/axion/fukachan/work/spool/EXP/fml.pl", "(fml)", NULL);
-  exit(1);
+  setgid(getgid());
+  execl("/home/axion/fukachan/work/spool/EXP/fml.pl", /* where is fml.pl */
+	"(fml)", 
+	"/home/axion/fukachan/work/spool/EXP", /* where is config.ph */
+	"/home/axion/fukachan/work/spool/EXP", /* library of fml package */
+	NULL);
+  exit(0);
 }
