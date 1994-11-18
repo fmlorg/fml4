f2b964fc5e5fc8db6ffd5318bdede8b4413d3442
fukachan <fukachan>
1994-11-18 16:02:44 +0000

    final version for VA contest

--- contrib/localhack/release-1/Elena/vote.pl
+++ contrib/localhack/release-1/Elena/vote.pl
@@ -2,7 +2,9 @@
 eval "exec /usr/local/bin/perl -S $0 $*"
     if $running_under_some_shell;
 
-$voteid = q$Id$;
+$rcsid = q$Id$;
+($rcsid) = ($rcsid =~ /Id:(.*).pl,v(.*) *\d\d\d\d\/\d+\/\d+.*/ && $1.$2);
+$STATUS = "vote.pl [$rcsid] ";
 
 # For the insecure command actions
 $ENV{'PATH'}  = '/bin:/usr/ucb:/usr/bin';	# or whatever you need
@@ -11,8 +13,10 @@ $ENV{'IFS'}   = '' if $ENV{'IFS'} ne '';
 
 ###### MAIN #####
 require 'getopts.pl';
-&Getopts('D:hd');
+&Getopts('D:hdS:t');
 $DIR = $opt_D ? $opt_D : $ENV{'PWD'};
+$TODAY = 1 if $opt_t;
+die("USAGE: $0 -[DhdSt] files\n") if $opt_h;
 
 umask (022);
 require 'vote.ph';
@@ -20,12 +24,45 @@ $debug = 1 if $opt_d;
 
 &VoteInitConfig;
 
+if($opt_S) {
+    $SUMMARY_MODE = 1;
+    local($a, $b) = split(/\-/, $opt_S);
+    foreach ($a .. $b) {
+	local($file) = "$DIR/spool/$_";
+	print STDERR "$file\n";
+	&Vote(&GetBufferFromFile($file)) if -f $file;
+    }
+    &VoteOutput;
+    exit 0;
+}
+
 if($0 =~ 'summary.pl') { 
     $SUMMARY_MODE = 1;
-    foreach (<$DIR/spool/[0-9]>, <$DIR/spool/[0-9][0-9]>, 
-	     <$DIR/spool/[0-9][0-9][0-9]>) { # built-in is a special?
-	&Vote(&GetBufferFromFile($_)) if(/spool\/\d+$/);
+
+    if($TODAY) {# today's status report
+	$TODAY = sprintf("%2d/%02d/%02d", $year, $mon + 1, $mday);
+	print STDERR "TODAY $TODAY\n\n";
+	if(open(TMP, "$DIR/summary")) {
+	    while(<TMP>) {
+		if(/^$TODAY \d\d:\d\d:\d\d \[(\d+)/) {
+		    push(@TODAY, "spool/$1");
+		}
+	    }
+	    close(TMP);
+	}
+
+	foreach (@TODAY) {
+	    print STDERR "TODAY $_\n";
+	    &Vote(&GetBufferFromFile($_)) if(/spool\/\d+$/);
+	}
+
+    }else {
+	foreach (<$DIR/spool/[0-9]>, <$DIR/spool/[0-9][0-9]>, 
+		 <$DIR/spool/[0-9][0-9][0-9]>) { # built-in is a special?
+	    &Vote(&GetBufferFromFile($_)) if(/spool\/\d+$/);
+	}
     }
+
     &VoteOutput;
     exit 0;
 }
@@ -45,6 +82,7 @@ exit 0;
 sub VoteLog
 {
     local($strings) = @_;
+
     open(FILE, ">> $LOGFILE");
     print FILE $Date, ":", $strings, "\n";
     close(FILE);
@@ -53,8 +91,9 @@ sub VoteLog
 sub VoteInitConfig
 {
     @WDay = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
-    @Month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
-	      'Sep', 'Oct', 'Nov', 'Dec');
+    @Month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+	      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+    
     ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
     $Date = sprintf("%s, %d %s %d %02d:%02d:%02d %s", $WDay[$wday],
 		    $mday, $Month[$mon], $year, $hour, $min, $sec, $TZone);
@@ -69,14 +108,13 @@ sub VoteInitConfig
 sub GetBufferFromFile
 {
     local($FD) = @_;
-    local($BUFFER) = "";
-    local($WHOLE_MAIL);
-    local($MailHeaders) = "";
+    local($MailHeaders, $BUFFER, $WHOLE_MAIL);
 
     open(FD, "$JCONVERTER $FD|") || (&VoteLog("cannot open $FD"), return);
     eval "while(<FD>) {
 	    if(1 .. /^\$/) {
 	        \$MailHeaders .= \$_;
+                next;
 	    };
 
 	    $READ_FILE_Hook;
@@ -86,7 +124,9 @@ sub GetBufferFromFile
     close(FD);
 
     &GetFrom($MailHeaders);
-    $Original_From_address = &DecodeMimeStrings($Original_From_address);
+    if($USE_LIBMIME) {
+	$Original_From_address = &DecodeMimeStrings($Original_From_address);
+    }
 
     return split(/\n/, $WHOLE_MAIL, 9999);
 }
@@ -127,27 +167,37 @@ sub GetFrom
 	
     }	# end of while loop
 
+    $Summary{$ID} = "$ID\t$From_address"; # for summary report
+
     return $Original_From_address;
 }
 
-# get one buffer, collect @keywords up to @maxkeywords. 
+# get one buffer, collect @keyword up to %maxkeywords. 
 sub Vote {
-    
-    local(@votebody) = @_;
     local($i) = local($j) = 0;
     local($COMMAND, $GET_FIELD, $JNAMECONV, $RESET, $AsA2A);
 
     # generate regexp for given fields(One Line Matching)
-    foreach $key (@keywords) {
+    foreach $key (@keyword) {
+	# Reset Sequence
 	$RESET .= "\$Count{$key} = 0;\n";
 	$RESET .= "undef %Var$key;\n";
-	$JNAMECONV .= "\ts/^$Jname{$key}/$key/;\n" if $Jname{$key};
 
-	$GET_FIELD .= "if((/^$key:(.*)\\(/o || /^$key:(.*)/o )";
-	$GET_FIELD .= "&& \$Count{$key}++ < $maxkeywords[$i])\n\t";
-	$GET_FIELD .= "{ \$Var$key{\$1} += 1;}";
-	$GET_FIELD .= "\n\t";
+	# Jname conversion sequence
+	$JNAMECONV .= "s/^$Jname{$key}/$key/;\n\t" if $Jname{$key};
 
+	# Get fields sequence, a little tricky
+	$GET_FIELD .= "if(/^$key:/o) { \$Sum$key .= \$_.\"\\n\";}\n\t";
+#	$GET_FIELD .= "if((/^$key:(.*)\\(/o || /^$key:(.*)/o )";
+	$GET_FIELD .= "if(/^$key:(.*)\\(/o) { \$NonEffective$key += 1; next;};\n\t";
+	$GET_FIELD .= "if(/^$key:(.*)/o ";
+	$GET_FIELD .= "&& \$Count{$key}++ < $maxkeyword{$key}) {\n\t";
+	$GET_FIELD .= "\t\$Var$key{\$1} += 1;\n\t";
+	$GET_FIELD .= "\t\$Effective$key += 1;";
+	$GET_FIELD .= "\n\t}";
+	$GET_FIELD .= "\n\n\t";
+
+	# Asossiation Array -> just array
 	$AsA2A     .= "\n\t";
 	$AsA2A     .= "foreach(keys %Var$key) { \$$key{\$_} += 1;}\n";
 	$i++;
@@ -155,7 +205,7 @@ sub Vote {
 
     # Execution Command Generation
     $EXEC_COMMAND = "$RESET
-    foreach (\@votebody) {
+    foreach (\@_) {
         $Hook
 
  	# get voting fields, initilize
@@ -168,13 +218,15 @@ sub Vote {
         $JNAMECONV\n\t# Jname and Name conversion
         $GET_FIELD\n
     }
-    $AsA2A;\n";
+
+    $AsA2A;
+    ";
+
     $I++;
     
     print STDERR $EXEC_COMMAND if $debug;
     eval $EXEC_COMMAND;
     print STDERR $@ if $@;
-
 }
 
 sub VoteCleanup
@@ -186,21 +238,63 @@ sub VoteCleanup
 sub VoteOutput {
     local($i) = 0;
 
-    # generate evaled strings
-    foreach $key (@keywords) {
+    print "$STATUS Status Report\n---\n";
+
+    if($SUMMARY_MODE) {
+	print "[Summary] person list who voted\nID\tvoter\n\n";
+
+	foreach (keys %Summary) { # summary Report
+	    print $Summary{$_}, "\n";
+	}
+
+	print "\n---\n\n";
+    }
+
+    foreach $key (@keyword) {
 	$title = $Jname{$key} ? $Jname{$key} : $key;
+	if(!$SUMMARY_MODE) {
+	    $GET_FIELD .= 
+		"\tforeach(keys \%$key) {
+                 \tpush(\@$key, \"\$_\");
+            \t};\n\t
+            \$Effective$key = scalar(\@$key);
+            ";
+	}else {
+	    $GET_FIELD .= 
+		"\tforeach(keys \%$key) {
+                 \tpush(\@$key, \"\$$key{\$_}\t\$_\");
+            \t};\n\t
+            ";
+	}
+
+	$GET_FIELD .= "print \"$title 有効票:\t\$Effective$key\\n\";\n\t";
+	$GET_FIELD .= "\$NonEffective$key = 0 unless \$NonEffective$key;\n\t";
+	$GET_FIELD .= "print \"$title 無効票:\t\$NonEffective$key\\n\";\n\t";
+    }
+
+    $GET_FIELD .= "print \"\\n\";\n\t";
+
+    # generate evaled strings
+    foreach $key (@keyword) {
+	$title = $Jname{$key} ? $Jname{$key} : $key;
+
 	$GET_FIELD .= 
-	    "foreach(keys \%$key) { 
-                 push(\@$key, \"\$$key{\$_} \$_\");
-            }
-            print \"$title\\n\\n\t\", 
+	    "print \"$title\\n\\n\t\", 
                   join(\"\\n\\t\", sort {\$b <=> \$a;} \@$key), \"\\n\\n\";
             ";
     }
 
-    print STDERR ">",$GET_FIELD . $HookOut,"<\n" if $debug;
-    eval $GET_FIELD . $HookOut;
+    if($SUMMARY_MODE) {
+	$S_GET_FIELD .= "print \"---\n投票データ一覧\\n\\n\";\n";
+	foreach $key (@keyword) {
+	    $S_GET_FIELD .= "print \$Sum$key;\n";
+	}
+    }
+
+    print STDERR "-----\n",$GET_FIELD . $HookOut,"\n-----\n" if $debug;
+    eval $GET_FIELD . $S_GET_FIELD . $HookOut;
     print STDERR $@ if $@;
+
 }
 
 1;
