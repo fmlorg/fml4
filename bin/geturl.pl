5640e7daa9ede0cdf2913d45b9a76d3148ce79fd
fukachan <fukachan>
1995-11-02 08:00:17 +0000

    *** empty log message ***

--- bin/geturl.pl
+++ bin/geturl.pl
@@ -1,26 +1,55 @@
 #!/usr/local/bin/perl
 
-local($req) = (shift @ARGV) || 'http://www.phys.titech.ac.jp/uja/';
-
-$DIR = $TMP_DIR = $ENV{'TMPDIR'} || '.';
-
-$TAR		= "/usr/bin/tar cf -";
-$UUENCODE	= "/bin/uuencode";
-$RM		= "/bin/rm -fr";
-$CP		= "/bin/cp";
-$COMPRESS	= "/usr/bin/gzip -c";
-$ZCAT		= "/usr/bin/zcat";
-
-# SPECIAL
-$UUENCODE = "/bin/cat";
-
 require 'libhref.pl';
 
-&Href($req, *e);
+$DIR     = $TMP_DIR    = $ENV{'TMPDIR'} || '.';
+$req     = shift @ARGV || 'http://www.phys.titech.ac.jp/uja/';
+$outfile = shift @ARGV;
+$debug   = 1;
 
-print $e{'message'};
+if ($outfile) {
+    -f $outfile && die("$outfile already exists, exit!\n");
+    ($outfile eq '-') && ($STDOUT = 1);
+} 
+else {
+    ($req =~ m@\S+/(\S+)@) && ($outfile = $1);
+}
 
-sub Log   { print STDERR " @_ \n";}
-sub Debug { &Log(@_);}
+
+##### Retrieve
+$e{'special:geturl'} = 1;
+
+&HRef($req, *e);
+
+$tmpf = $e{'special:geturl'};
+undef $e{'special:geturl'};
+
+##### $tmpf -> $outfile
+open(IN, $tmpf) || die("< $tmpf: $!\n");
+
+if (! $STDOUT) {
+    open(STDOUT, "> $outfile") || die("> $outfile: $!\n");
+}
+else {
+    &Log("> STDOUT");
+}
+
+select(STDOUT); $| = 1;
+
+while (sysread(IN, $_, 4096)) { print $_;}
+
+close(STDOUT);
+close(IN);
+
+&Log("$tmpf -> ".($outfile || 'STDOUT'));
+unlink $tmpf;
+
+exit 0;
+
+
+### Library
+sub Log     { print STDERR "LOG>@_\n";}
+sub Debug   { &Log(@_);}
+sub LogWEnv { &Log(@_);}
 
 1;
