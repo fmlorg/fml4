9c54685815737e808fed0662ba9519fbed8ecb03
fukachan <fukachan>
1995-11-12 17:29:48 +0000

    *** empty log message ***

---
+++ proc/libremote.pl
@@ -0,0 +1,11 @@
+# Admin Commands 
+# Remote control library of fml  -> libra.pl
+#
+
+# local($id);
+# $id = q$Id$;
+# $rcsid .= " :".($id =~ /Id: lib(.*).pl,v\s+(\S+)\s+/ && $1."[$2]");
+
+eval "require 'libra.pl';";	# backward compatiblity;
+
+1;
