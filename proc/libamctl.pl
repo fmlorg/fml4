50d74f92ee11b31b80eb6cacf9068c3c40fb0b84
fukachan <fukachan>
1996-12-05 09:32:31 +0000

    Initial revision

---
+++ proc/libamctl.pl
@@ -0,0 +1,686 @@
+# List Modification functions
+#
+# Copyright (C) 1994-1996 fukachan@phys.titech.ac.jp
+# Copyright (C) 1996      fukachan@sapporo.iij.ad.jp
+# fml is free software distributed under the terms of the GNU General
+# Public License. see the file COPYING for more details.
+
+
+local($id);
+$id = q$Id$;
+$rcsid .= " :".($id =~ /Id: lib(.*).pl,v\s+(\S+)\s+/ && $1."[$2]");
+
+
+# Auto registraion procedure
+# Subject: subscribe
+#        or 
+# subscribe ... in Body.
+# return 0 or 1 to use the return of &MLMemberCheck
+sub AutoRegist
+{
+    local(*e, $set_addr) = @_;
+    local($from, $s, $b, $r, @s);
+    local($file_to_regist) = $FILE_TO_REGIST || $MEMBER_LIST;
+
+    # &AutoRegist("address");
+    if ($set_addr) { undef $REQUIRE_SUBSCRIBE; $from = $set_addr;}
+
+    # for &Notify,  reply-to ? reply-to : control-address
+    $e{'h:Reply-To:'} = $e{'h:reply-to:'} || $e{'CtlAddr:'};
+
+    if ($REQUIRE_SUBSCRIBE && (! $REQUIRE_SUBSCRIBE_IN_BODY)) {
+	# Syntax e.g. "Subject: subscribe"...
+	# 
+	# [\033\050\112] is against a bug in cc:Mail
+	# patch by yasushi@pier.fuji-ric.co.jp
+	$s = $e{'h:Subject:'};
+	$s =~ s/(^\#[\s\n]*|^[\s\n]*)//;
+	$s =~ s/^\033\050\112\s*//;
+	@s = split(/\n/, $s); # GET THE FIRST LINE ONLY
+	$s = shift @s;
+
+	&Debug("AUTO REGIST SUBJECT ENTRY [$s]") if $debug;
+
+	if ($s =~ /^$REQUIRE_SUBSCRIBE\s+(\S+).*/i) { 
+	    $from = $1;
+	    &Debug("AUTO REGIST FROM ENTRY [$from]") if $debug;
+	    $from =~ s/^(\S+).*/$1/;
+	    &Debug("AUTO REGIST FROM ENTRY [$from]") if $debug;
+	}
+	elsif ($s =~ /^$REQUIRE_SUBSCRIBE\s*/i) { 
+	    ;
+	}
+	else {
+	    &Debug("AUTO REGIST SUBJECT[$s] SYNTAX ERROR") if $debug;
+	    $sj = "Bad syntax subject in autoregistration";
+	    $b  = "Subject: $REQUIRE_SUBSCRIBE [your-email-address]\n";
+	    $b .= "\t[] is optional\n";
+	    $b .= "\tfor changing your address to regist explicitly.\n";
+
+	    &Log($sj, "Subject => [$s]");
+	    &Warn("$sj $ML_FN", &WholeMail);
+
+	    # notify
+	    $e{'message:h:subject'} .= $sj;
+	    $e{'mode:stranger'}      = 1;
+	    &Mesg(*e, $b.&WholeMail);
+
+	    return 0;
+	}
+    }
+    elsif ($REQUIRE_SUBSCRIBE && $REQUIRE_SUBSCRIBE_IN_BODY) {
+	# Syntax e.g. "subscribe" in body
+	$s = $e{'Body'};
+	$s =~ s/(^\#[\s\n]*|^[\s\n]*)//;
+	$s =~ s/^\033\050\112\s*//;
+	@s = split(/\n/, $s); # GET THE FIRST LINE ONLY
+	$s = shift @s;
+
+	&Debug("AUTO REGIST BODY ENTRY [$s]") if $debug;
+
+	if ($s =~ /^$REQUIRE_SUBSCRIBE\s+(\S+).*/i) { 
+	    $from = $1;
+	    &Debug("AUTO REGIST FROM ENTRY [$from]") if $debug;
+	    $from =~ s/^(\S+).*/$1/;
+	    &Debug("AUTO REGIST FROM ENTRY [$from]") if $debug;
+	}
+	elsif ($s =~ /^$REQUIRE_SUBSCRIBE\s*/i) { 
+	    ;
+	}
+	else {
+	    &Debug("AUTO REGIST BODY[$s] SYNTAX ERROR") if $debug;
+	    $sj = "Bad syntax body in autoregistration";
+	    $b  = "Body: $REQUIRE_SUBSCRIBE [your-email-address]\n";
+	    $b .= "\t[] is optional\n";
+	    $b .= "\tfor changing your address to regist explicitly.\n";
+
+	    &Log($sj, "Body => [$s]");
+	    &Warn("$sj $ML_FN", &WholeMail);
+
+	    # notify
+	    $e{'message:h:subject'} .= $sj;
+	    &Mesg(*e, $b.&WholeMail);
+
+	    return 0;
+	}
+    }
+    else {
+	# DEFAULT
+	# Determine the address to check
+	# In default, when changing your registered address
+	# use "subscribe your-address" in body.
+	$s = $set_addr || $e{'Body'};
+	$s =~ s/(^\#\s*|^\s*)//;
+	
+	$DEFAULT_SUBSCRIBE || ($DEFAULT_SUBSCRIBE = "subscribe");
+
+	if ($s =~ /^${DEFAULT_SUBSCRIBE}\s+(\S+)\s*/i) { 
+	    $from = $1;
+	    &Debug("AUTO REGIST DEFAULT ENTRY [$from]") if $debug;
+	    $from =~ s/^(\S+).*/$1/;
+	    &Debug("AUTO REGIST DEFAULT ENTRY [$from]") if $debug;
+	}
+    }# end of REQUIRE_SUBSCRIBE;
+	
+    &Debug("AUTO REGIST CANDIDATE>$from<") if $debug;
+    $from = ($from || $From_address);
+    &Debug("AUTO REGIST FROM     >$from<") if $debug;
+
+    return 0 if     &LoopBackWarn($from); 	# loop back check	
+    return 0 unless &Chk822_addr_spec_P($from);	# permit only 822 addr-spec 
+
+    ### duplicate check (patch by umura@nn.solan.chubu.ac.jp 95/06/08)
+    if (&CheckMember($from, $file_to_regist)) {	
+	&Log("AutoRegist: Dup $from");
+	&Mesg(*e, "Address [$from] already subscribed.");
+	&Mesg(*e, &WholeMail);
+	return 0;
+    }
+
+    ##### ADD the newcomer to the member list
+    local($ok, $er);		# ok and error-strings
+
+    # WHEN CHECKING MEMBER...
+    if ($ML_MEMBER_CHECK) {
+	&Append2($from, $file_to_regist) ? $ok++ : ($er  = $file_to_regist);
+	&Append2($from, $ACTIVE_LIST)    ? $ok++ : ($er .= " $ACTIVE_LIST");
+	($ok == 2) ? &Log("Added: $from") : do {
+	    &Warn("ERROR[sub AutoRegist]: cannot operate $er", &WholeMail);
+	    return 0; 
+	};
+    }
+    # AUTO REGISTRATION MODE
+    else {
+	&Append2($from, $file_to_regist) ? $ok++ : ($er  = $file_to_regist);
+	$ok == 1 ? &Log("Added: $from") : do {
+	    &Warn("ERROR[sub AutoRegist]: cannot operate $er", &WholeMail);
+	    return 0;
+	};
+    }
+
+    ### WHETHER DELIVER OR NOT;
+    # 7 is body 3 lines and signature 4 lines, appropriate?;
+    # spelling miss fix;
+    if (defined $AUTO_REGISTERD_UNDELIVER_P) {
+        $AUTO_REGISTERED_UNDELIVER_P = $AUTO_REGISTERD_UNDELIVER_P;
+    }
+
+    local($limit) = $AUTO_REGISTRATION_LINES_LIMIT || 8;
+    &Log("Deliver? $e{'nlines'} <=> $limit") if $debug;
+    &Log("\$AUTO_REGISTERED_UNDELIVER_P is $AUTO_REGISTERED_UNDELIVER_P");
+
+    if ($e{'nlines'} < $limit) { 
+	&Log("Not deliver: lines:$e{'nlines'} < $limit");
+	$r  = "The number of mail body-line is too short(< $limit),\n";
+	$r .= "So NOT FORWARDED to ML($MAIL_LIST). O.K.?\n";
+	$r .= "(FYI: \$AUTO_REGISTERED_UNDELIVER_P is set)\n" 
+	    if $AUTO_REGISTERED_UNDELIVER_P;
+	$r .= "\n".('-' x 30) . "\n\n";
+	$AUTO_REGISTERED_UNDELIVER_P = 1;
+    }
+    elsif ($AUTO_REGISTERED_UNDELIVER_P) {
+	$r  = "\$AUTO_REGISTERED_UNDELIVER_P is set, \n";
+	$r .= "So NOT FORWARDED to ML($MAIL_LIST).\n\n";
+	$r .= ('-' x 30) . "\n\n";
+    }
+    
+    &Warn("New added member: $from $ML_FN", $r . &WholeMail);
+    &SendFile($from, $WELCOME_STATEMENT, $WELCOME_FILE);
+
+    ### Ends.
+    ($AUTO_REGISTERED_UNDELIVER_P ? 0 : 1);
+}
+
+
+# check addr-spec in RFC822
+# patched by umura@nn.solan.chubu.ac.jp 95/6/8
+# return 1 if O.K. 
+sub Chk822_addr_spec_P
+{
+    local($from) = @_;
+
+    if ($from !~ /\@/) {
+	&Log("NO \@ mark: $from");
+        &Mesg(*Envelope, "WARNING ON AUTO-REGISTRATION::Chk822_addr_spec_P:");
+        &Mesg(*Envelope, "Address [$from] contains NO '\@' STRING, SO EXIT!");
+        &Mesg(*Envelope, "[$from\@$FQDN] FORM IS REQUIRED for uniqueness\n");
+        &Mesg(*Envelope, "*** Please use e.g. [subscribe $from\@$FQDN] FORM");
+        &Mesg(*Envelope, "***        OR                                ");
+        &Mesg(*Envelope, "*** Manually Edit for $from on the localhost ");
+	&Mesg(*Envelope, &WholeMail);
+	return 0;
+    }
+
+    1;
+}
+
+
+########################################################################
+
+
+sub DoSetDeliveryMode
+{
+    local($proc, *Fld, *e, *misc) = @_;
+    local($c, $_, $cmd, $opt, $curaddr);
+
+    &Debug("\$curaddr = $misc || $Addr || $From_address");
+
+    $curaddr = $misc || $Addr || $From_address; # address to operate;
+    $cmd     = $proc; $cmd =~ tr/a-z/A-Z/; $_ = $cmd;
+    $opt     = $Fld[2];
+
+    ### LOOP CHECK ###
+    if (&LoopBackWarn($curaddr)) {
+	&Log("$cmd: LOOPBACK ERROR, exit");
+	return $NULL;
+    }
+
+    ###### Matome Okuri prescan
+    # [case 1 "matome 3u"]
+    if (/^(MATOME|DIGEST)$/ && ($opt =~ /^(\d+|\d+[A-Za-z]+)$/)) {
+	# set matome-okuri-parameter. *Fld -> &Change..( , , , *opt);
+	$c = $opt;
+
+	# Exception is synchronous delivery
+	if (0 == $c) {
+	    $c = " -> Synchronous Delivery";
+	}
+	# e.g. 6 , 6u , ...
+	else {
+	    # search this mode exists?
+	    &use('utils');
+	    local($d, $mode) = &ModeLookup($c);
+
+	    if ((!$d) && (!$mode)) { 
+		&Log("$cmd $c fails, not match");
+		&Mesg(*e, "$cmd: $opt parameter not match.");
+		&Mesg(*e, "\tDO NOTHING!");
+		return $NULL;
+	    }			
+	}# $c == non-nil;
+
+	$cmd = "MATOME";
+	&Log("O.K. Try $cmd $c");
+    }
+    ###### [case 2 "matome" call the default slot value]
+    elsif (/^(MATOME|DIGEST)$/i) {
+	&Log("$cmd: $opt syntax is inappropriate, do nothing");
+	&Mesg(*e, "$cmd: $opt syntax is inappropriate.");
+	&Mesg(*e, "\t$cmd require someting as an option") if $opt eq "";
+	&Mesg(*e, "\tDO NOTHING!");
+	return $NULL;
+    }
+
+    if (&ChangeMemberList($cmd, $curaddr, $ACTIVE_LIST, *opt)) {
+	&Log("$cmd [$curaddr] $c");
+	&Mesg(*e, "$cmd [$curaddr] $c accepted.");
+    }
+    else {
+	&Log("$cmd [$curaddr] $c failed");
+	&Mesg(*e, "$cmd [$curaddr] $c failed.");
+    }
+}
+
+
+sub DoSetMemberList
+{
+    local($proc, *Fld, *e, *misc) = @_;
+    local($curaddr, $newaddr);
+
+    $cmd = $proc; $cmd =~ tr/a-z/A-Z/; $_ = $cmd;
+
+    # {3u,oldaddr} newaddr 
+    $curaddr = $Fld[2] || $Addr || $From_address;#(From_address when bye)
+    $newaddr = $Fld[3];
+
+    &Debug("\n   SetMemberList::(\n\tcur  $curaddr\n\tnew  $newaddr\n\tmisc $misc\n") if $debug;
+
+    # KEYWORD for 'chaddr'
+    $CHADDR_KEYWORD = $CHADDR_KEYWORD || 'CHADDR|CHANGE\-ADDRESS|CHANGE';
+
+    # LOOP CHECK
+    if (&LoopBackWarn($curaddr)) {
+	&Log("$cmd: LOOPBACK ERROR, exit");
+	return '';
+    }
+    
+    ###### change address [chaddr old-addr new-addr]
+    # Default: $CHADDR_KEYWORD = 'CHADDR|CHANGE\-ADDRESS|CHANGE';
+    if (/^($CHADDR_KEYWORD)$/i) {
+	&Mesg(*e, "\t set $cmd => CHADDR");
+	$cmd = 'CHADDR';
+	
+	# LOOP CHECK
+	&LoopBackWarn($newaddr) && &Log("$cmd: LOOPBACK ERROR, exit") && 
+	    (return $NULL);
+
+	#ATTENTION! $curaddr or $newaddr should be a member.
+	if (&CheckMember($curaddr, $MEMBER_LIST) || 
+	    &CheckMember($newaddr, $MEMBER_LIST)) {
+	    &Log("$cmd: OK! Either $curaddr and $newaddr is a member.");
+	    &Mesg(*e, "\tTry change\n\n\t$curaddr\n\t=>\n\t$newaddr\n");
+	}
+	else {
+	    &Log("$cmd: NEITHER $curaddr and $newaddr is a member. STOP");
+	    &Mesg(*e, "$cmd:\n\tNEITHER\n\t$curaddr nor $newaddr");
+	    &Mesg(*e, "\tis a member.\n\tDO NOTHING!");
+	    return 'LAST';
+	}
+    }
+    # NOT CHADDR;
+    else {
+	$newaddr = $curaddr; # tricky;
+	&Mesg(*e, "\t set $cmd => BYE");
+	$cmd = 'BYE';
+    }
+
+    ##### Call recursively
+    local($r) = 0;
+    if ($ML_MEMBER_CHECK) {
+	&ChangeMemberList($cmd, $curaddr, $MEMBER_LIST, *newaddr) && $r++;
+	&Log("$cmd MEMBER [$curaddr] $c O.K.")   if $r == 1 && $debug2;
+	&Log("$cmd MEMBER [$curaddr] $c failed") if $r != 1;
+    }
+    else {
+	$r++;
+    }
+
+    return $NULL if $Envelope{'mode:majordomo:chmemlist'};
+
+    &ChangeMemberList($cmd, $curaddr, $ACTIVE_LIST, *newaddr) && $r++;
+    &Log("$cmd ACTIVE [$curaddr] $c O.K.")   if $r == 2  && $debug2;
+    &Log("$cmd ACTIVE [$curaddr] $c failed") if $r != 2;
+
+    # Status
+    if ($r == 2) {
+	&Log("$cmd [$curaddr] $c accepted");
+	&Mesg(*e, "$cmd [$curaddr] $c accepted.");
+    }
+    else {
+	&Log("$cmd [$curaddr] $c failed");
+	&Mesg(*e, "$cmd [$curaddr] $c failed.");
+    }
+
+    return 'LAST';
+}
+
+
+# For convenience
+sub GenFmlHeader
+{
+q$#.FML HEADER
+# NEW FORMAT FOR FURTHER EXTENTION
+# e.g. fukachan@phys r=relayserver m=3u s=skip 
+# r= relayserver
+# m= matomeokuri parameter is time and option
+# s= skip. can post from this address but not delivered here.
+#
+# the same obsolete format is compatible with new format and as follows:
+# e.g. fukachan@phys relayserver matome 3u
+#.endFML HEADER
+$;
+}
+
+
+sub ChangeMemberList
+{
+    local($org_addr) = $ADDR_CHECK_MAX;	# save the present severity
+    local($status);
+
+    while ($ADDR_CHECK_MAX < 10) { # 10 is built-in;
+	$status = &DoChangeMemberList(@_);
+	last if $status ne 'RECURSIVE';
+	$ADDR_CHECK_MAX++;
+	&Debug("Call Again ChangeMemberList(...)[$ADDR_CHECK_MAX]") if $debug;
+    } 
+
+    $ADDR_CHECK_MAX = $org_addr; # reset;
+    $status;
+}
+
+
+# MAIN Routine of ChangeMemberList(cmd, address, file) 
+# If multiply matched for the given address, 
+# do Log [$log = "$addr"; $log_c++;]
+sub DoChangeMemberList
+{
+    local($cmd, $curaddr, $file, *misc) = @_;
+    local($status, $log, $log_c, $r, $addr, $org_addr, $addr_opt);
+    local($acct) = split(/\@/, $curaddr);
+
+    &Debug("DoChangeMemberList($cmd, $curaddr, $file, $misc)") if $debug;
+    
+    ### File IO
+    # NO CHECK 95/10/19 ($MEMBER_LIST eq $file || $ACTIVE_LIST eq $file)
+    # Backup
+    open(BAK, ">> $file.bak") || (&Log($!), return $NULL);
+    select(BAK); $| = 1; select(STDOUT);
+    print BAK "----- Backup on $Now -----\n";
+
+    # New
+    open(NEW, ">  $file.tmp") || (&Log($!), return $NULL);
+    select(NEW); $| = 1; select(STDOUT);
+
+    # Input
+    open(FILE,"<  $file") || (&Log($!), return $NULL);
+
+
+    ### Process GO!
+    print NEW &GenFmlHeader;
+
+    # $c == conserve comment;
+    local($c, $rcpt, $o);
+
+    in: while (<FILE>) {
+	chop;
+
+	print STDERR "TRY       [$_]\n" if $debug;
+
+	# If allow all people to post, OK ends here.
+	if (/^\+/o) { 
+	    &Log("NO CHANGE[$file] when no member check");
+	    close(FILE); 
+	    return ($status = 'done'); 
+	}
+
+	# Backward Compatibility.	tricky "^\s".
+	next in if /^#\.FML/o .. /^\#\.endFML/o;
+	if (! /^\#/o) {
+	    undef $c;		# reset comment;
+
+	    s/\s(\#.*)$/$c = $2/e; # comment extension 96/10/08,16;
+	    s/\smatome\s+(\S+)/ m=$1 /i;
+	    s/\sskip\s*/ s=skip /i;
+
+	    ($rcpt, $o) = split(/\s+/, $_, 2);
+	    $o = ($o && !($o =~ /^\S=/)) ? " r=$o " : " $o ";
+	    $_ = "$rcpt $o $c";
+	    s/\s+$/ /g;
+	}
+
+	print STDERR "TRY DChML:[$_]\n" if $debug;
+
+	# Backup
+	print BAK "$_\n";
+	next in if /^\s*$/o;
+
+	# get $addr for ^#\s+$addr$. if ^#, skip process except for 'off' 
+	$addr = '';
+	if (/^\s*(\S+)\s*(.*)/o)   { $addr = $1;}
+	if (/^\#\s*(\S+)\s*(.*)/o) { $addr = $1;}
+
+	# for high performance
+	if ($addr !~ /^$acct/i) {
+	    print NEW "$_\n"; 
+	    next in;
+	} 
+	elsif (! &AddressMatch($addr, $curaddr)) {
+	    print NEW "$_\n"; 
+	    next in;
+	}
+
+	# if matched, get "$addr including mx or comments"
+	if (/^\s*(.*)/o)   { $addr = $1;}
+	if (/^\#\s*(.*)/o) { $addr = $1;}
+	
+	print STDERR "DChML main::{ CMD=$cmd \$addr=[$addr]}\n" if $debug;
+
+	# not use "last" for the possibility the address is written double. 
+	# may not be effecient.
+	if ($cmd =~ /^ON|OFF|BYE|SKIP|NOSKIP|MATOME|CHADDR$/) {
+	    # Return to the ML
+	    print NEW "$addr\n" 	if $cmd eq 'ON';
+
+	    # Good Bye to the ML temporarily
+	    print NEW "\#\t$addr\n"     if $cmd eq 'OFF';
+
+	    # Good Bye to the ML eternally
+	    print NEW "\#\#BYE $addr\n" if $cmd eq 'BYE';
+
+	    # Address to SKIP
+	    print NEW "$addr\ts=skip\n" if $cmd eq 'SKIP';
+
+ 	    # Delete SKIP
+	    if ($cmd eq 'NOSKIP') {
+		$addr =~ s/\ss=(\S+)//ig; # remover s= syntax
+		print NEW "$addr\n"; 
+	    }
+
+	    # Matome Okuri Control
+	    if ($cmd eq 'MATOME') {
+		local($addr, $org_addr) = &CtlMatome($addr, *misc);
+		print NEW "$addr\n"; 
+	    }
+
+	    # Matome Okuri Control
+	    if ($cmd eq 'CHADDR') {
+		&Log("ChangeMemberList:$addr -> $misc");
+		if ($addr =~ /^(\S+)\s*(.*)/o) { $addr = $1; $addr_opt = $2;}
+		print NEW "$misc $addr_opt\n"; 
+	    }
+
+	    $status = 'done'; 
+	    $log .= "$cmd $addr; "; $log_c++;
+	}# CASE of COMMANDS;
+	else {
+	    print NEW "$_\n"; 
+	    &Log("ChangeMemberList:Unknown cmd = $cmd");
+	}
+    } # end of while loop;
+
+    # CORRECTION; If not registerd, add the Address to SKIP
+    if ($cmd eq 'SKIP' && $status ne 'done') { 
+	print NEW "$curaddr\ts=skip\n"; 
+	$status = 'done'; 
+    }
+
+    # END OF FILE OPEN, READ..
+    close(BAK); close(NEW); close(FILE);
+
+    # protection for multiplly matching, 
+    # $log_c > 1 implies multiple matching;
+    # ADMIN MODE permit multiplly matching($_cf{'mode:addr:multiple'} = 1);
+    ## IF MULTIPLY MATCHED
+    if ($log_c > 1 && 
+	($ADDR_CHECK_MAX < 10) && 
+	(! $_cf{'mode:addr:multiple'})) {
+	&Log("$cmd: Do NOTHING since Muliply MATCHed..");
+	$log =~ s/; /\n/g;
+	&Mesg(*e, "Multiply Matched?\n$log");
+	&Mesg(*e, "Retry to check your adderss severely");
+
+	# Recursive Call
+	return 'RECURSIVE';
+    }
+    ## IF TOO RECURSIVE
+    elsif ($ADDR_CHECK_MAX >= 10) {
+	&Log("MAXIMUM of ADDR_CHECK_MAX, STOP");
+    }
+    ## DEFAULT 
+    else {
+	rename("$file.tmp", $file) || 
+	    (&Log("fail to rename $file"), return $NULL);
+    }
+
+    # here should be "Only once called"
+    if ($cmd eq 'MATOME' && $status eq 'done') {
+	&Log("ReConfiguring $curaddr in \$MSendRC");
+	&ConfigMSendRC($curaddr);
+	&Rehash($org_addr) if $org_addr;# info of original mode is required
+    }
+
+    &Mesg(*e, "O.K.!") if $status eq 'done';
+
+    $status;
+}
+    
+
+sub CtlMatome
+{
+    local($a, *m)    = @_;
+    local($matome)   = $m;	# set value(0 implies Realtime deliver)
+    local($org_addr) = $a;	# save excursion
+    local($s);
+
+    &Debug("CtlMatome { $addr=[$addr] => [$matome];}") if $debug;
+
+    # parameter is whether 0 or not-defiend
+    if ($matome eq '') {
+	# modification
+	if ($addr =~ /\smatome/oi || $addr =~ /\sm=/) {
+	    $matome = 'RealTime';
+	}
+	# new comer, set default
+	else {
+	    $matome = 3;
+	    $s = "Hmm.. no given parameter. use default[m=3]";
+	    &Log($s);
+	    &Mesg(*e, "$s");
+	    &Mesg(*e, "So your request is accepted but modified to m=3");
+	}
+    }
+    elsif ($matome == 0) {
+	$matome = 'RealTime';
+    }
+
+    # Remove the present matomeokuri configuration
+    $addr =~ s/^(.*)matome/$1/ig; # backward compatibility
+    $addr =~ s/\sm=(\S+)//ig; # remover m= syntax
+
+    # Set value
+    if ($matome eq 'RealTime') {    # 'call &Rehash'
+	;
+    }
+    else {
+	$addr = "$addr\tm=$matome";
+	undef $org_addr;	# 'NOT require call &Rehash'
+    }
+
+    # return value
+    ($addr, $org_addr);
+}
+
+
+# Send mails left in spool for "# matome 0".
+sub Rehash
+{
+    local($adr) = @_;
+    local($l, $r, $s, $d, $mode);
+    ($adr, $mode) = split(/\s+/, $adr, 2);
+
+    print STDERR "\n---Rehash local($adr, $mode)\n\n";
+
+    &use('utils');
+
+    if ($mode =~ /m=(\S+)/) {
+	($d, $mode) = &ModeLookup($1);
+    }
+    else {
+	($d, $mode) = &ModeLookup('');
+    }
+
+    $r = &GetID;		# next ID when delivery
+    $l = &GetPrevID($adr);	# next ID when msend
+
+    if ($l < $r) {
+	$s = "Rehash: Try send mails[$l - $r] left in spool.";
+	$_cf{'rehash'} = "$l-$r"; # for later use "# rehash" ???
+	&Log($s);
+	&Mesg(*e, "\n$s\n");
+    }
+    else { # if $l == $r, no article must be left
+	$s = "Rehash: no article to send you is left in spool.";
+	&Log($s);
+	&Mesg(*e, "\n$s\n");
+	return;
+    }
+
+    # make an entry 
+    local(@fld) = ('#', 'mget', "$l-$r", 10, $mode);
+
+    ($l <= $r) && &ProcMgetMakeList('Rehash:EntryIn', *fld);
+
+    1;
+}
+
+
+# for matomeokuri control
+# added the infomation to MSEND_RC
+# return NONE
+sub ConfigMSendRC
+{
+    local($curaddr) = @_;
+    local($ID) = &GetID;
+
+    if (open(TMP, ">> $MSEND_RC") ) {
+	select(TMP); $| = 1; select(STDOUT);
+	print TMP "$curaddr\t$ID\n";
+	close TMP;
+    } 
+    else { 
+	&Log("Cannot open $MSEND_RC");
+    }
+}
+
+
+1;
