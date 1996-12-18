dcebe0d0c8a32d645edac110a2df6acd6810d1d3
fukachan <fukachan>
1996-12-18 01:10:03 +0000

    interanctive stty -echo

--- bin/passwd.pl
+++ bin/passwd.pl
@@ -11,13 +11,16 @@ $rcsid   = q$Id$;
 $rcsid  .= 'Current';
 
 require 'getopts.pl';
-&Getopts("p:hdi");
+&Getopts("p:hdiI:");
 
 $opt_h && die(&Usage);
+@ARGV  || die(&Usage);
+
 $debug++ if $opt_d;
 $init++  if $opt_i;
 
 push(@INC, 'proc');
+push(@INC, $opt_I);
 require 'libcrypt.pl';
 
 &Log("Force to change password ...");
@@ -25,6 +28,34 @@ require 'libcrypt.pl';
 $PASSWD_FILE = $opt_p || 'etc/passwd';
 $to          = shift;
 $p           = shift;
+
+while (!$to || !$p) {
+    if (! $to) {
+	print "Address: ";
+	chop($to = <STDIN>);
+    }
+    else {
+	print "Address: $to\n";
+    }
+
+    if (! $p) {
+	# no echo
+	system "stty", "-echo";
+
+	print "Passwd: ";
+	chop($p = <STDIN>);
+	print "\n";
+
+	system "stty", "echo";
+    }
+
+    if (!$to || !$p) {
+	&Warn("Error: Please input NOT NULL Address and Password.");
+    }
+}
+
+
+if (!-f $PASSWD_FILE) { open(P, ">>$PASSWD_FILE"); close(P);}
 
 ($to && $p) || die("incorrect arguments?\n".&Usage);
 
@@ -43,8 +74,8 @@ exit 0;
 sub Usage
 {
     local($s) = q#;
-    passwd.pl [-i] [-p password-file] username new-password;
-    -i initialize;
+    passwd.pl [-i] [-p password-file] user password;
+    -i initialize password for the "user" as "password"; 
     -p alternative $PASSWD_FILE (default etc/passwd);
 
 #;
