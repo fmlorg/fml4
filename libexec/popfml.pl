c517c52a18aeaaf64a3456942e256c0ffad46775
fukachan <fukachan>
1997-01-12 07:30:33 +0000

    anyway beta test

--- libexec/popfml.pl
+++ libexec/popfml.pl
@@ -12,39 +12,43 @@ local($id);
 $id = q$Id$;
 $rcsid .= " :".($id =~ /Id: lib(.*).pl,v\s+(\S+)\s+/ && "$1[$2]");
 
-$DIR   =  $ENV{'PWD'};
-$debug = 1;
+$ENV{'PATH'}  = '/bin:/usr/ucb:/usr/bin';	# or whatever you need
+$ENV{'SHELL'} = '/bin/sh' if $ENV{'SHELL'} ne '';
+$ENV{'IFS'}   = '' if $ENV{'IFS'} ne '';
+
+# "Directory of Mailing List(where is config.ph)" and "Library-Paths"
+# format: fml.pl [-options] DIR(for config.ph) [PERLLIB's -options]
+# "free order is available" Now for the exist-check (DIR, LIBDIR) 
+foreach (@ARGV) { 
+    /^\-/   && &Opt($_) || push(@INC, $_);
+    $LIBDIR || ($DIR  && -d $_ && ($LIBDIR = $_));
+    $DIR    || (-d $_ && ($DIR = $_));
+}
+$DIR    = $DIR    || die "\$DIR is not Defined, EXIT!\n";
+$LIBDIR	= $LIBDIR || $DIR;
+$0 =~ m#(\S+)/(\S+)# && (unshift(@INC, $1)); #for lower task;
+unshift(@INC, $DIR); #IMPORTANT @INC ORDER; $DIR, $1(above), $LIBDIR ...;
 
 
-
-require 'libsmtp.pl';
+#################### MAIN ####################
 require 'proc/libkern.pl';
 require 'proc/libpop.pl';
 
+
+&CheckUGID;
+
+chdir $DIR || die "Can't chdir to $DIR\n";
+
+&InitConfig;			# Load config.ph, initialize conf, date,...
+
 &PopFmlInit;
-
-if (($pid = fork) < 0) {
-    &Log("Cannot fork");
-}
-elsif (0 == $pid) {
-    # go !
-    $status = &Pop'Gabble(*PopConf);#';
-    if ($status) { die "Error: $status\n";}
-    exit 0;
-}
-
-# Wait for the child to terminate.
-while (($dying = wait()) != -1 && ($dying != $pid) ){
-    ;
-}
-
-print STDERR "O.K. Go! Delivery.. \n";
-
+&PopFmlGabble;
+&PopFmlProg;
 
 exit 0;
+
+
 ############################################################
-
-
 sub PopFmlReadCF
 {
     local($h) = @_;
@@ -84,6 +88,9 @@ sub PopFmlGetopt
 
 sub PopFmlInit
 {
+    # for logging 
+    $From_address = "popfml";
+
     &PopFmlGetopt(@ARGV); 
     &PopFmlReadCF($PopConf{'SERVER'});
 
@@ -99,13 +106,74 @@ sub PopFmlInit
     }
 
     $PopConf{'TIMEOUT'}   = 30;
-    $PopConf{'QUEUE_DIR'} = './tmp/pop.queue';
-    $PopConf{'LOGFILE'}   = '/dev/stderr';
+    $PopConf{'QUEUE_DIR'} = 'var/pop.queue';
+    $PopConf{'LOGFILE'}   = 'var/log/_poplog';
     $PopConf{"PASSWORD"}  = $Password;
     $PopConf{"PROG"}      = "/usr/local/mh/lib/rcvstore";
 
     if (-f $ConfigFile) { require ($ConfigFile);}
 }
 
+
+sub PopFmlGabble
+{
+    if (($pid = fork) < 0) {
+	&Log("Cannot fork");
+    }
+    elsif (0 == $pid) {
+	# go !
+	$status = &Pop'Gabble(*PopConf);#';
+	if ($status) { die "Error: $status\n";}
+	exit 0;
+    }
+
+    # Wait for the child to terminate.
+    while (($dying = wait()) != -1 && ($dying != $pid) ){
+	;
+    }
+}
+
+
+sub PopFmlProg
+{
+    local($queue, $prog, $qf);
+
+    $queue = $PopConf{'QUEUE_DIR'};
+    $prog  = $PopConf{'PROG'};
+    $prog =~ s/^\|+//;
+
+    print STDERR "--PopFmlProg (queue=$queue prog=$prog)\n" if $debug;
+
+    opendir(DIRD, $queue) || &Log("Cannot opendir $queue");
+    for $qf (readdir(DIRD)) {
+	next if $qf =~ /^\./;
+
+	print STDERR "--PopFmlProg [$qf] ...\n" if $debug;
+
+	if (! open(QUEUE_IN, "$queue/$qf")) {
+	    &Log("Cannot open $queue/$qf");
+	    next;
+	}
+
+	if (! open(PROG_IN, "|$prog")) {
+	    &Log("Cannot exec [$prog]");
+	    next;
+	}
+
+	while (<QUEUE_IN>) { print PROG_IN $_;}
+
+	close(PROG_IN);
+	close(QUEUE_IN);
+
+	unlink "$queue/$qf" || 
+	    &Log("PopFmlProg: fails to remove the queue $qf");
+    }
+    closedir(DIRD);
+}
+
+
+### :include: -> libkern.pl
+# Getopt
+sub Opt { push(@SetOpts, @_);}
 
 1;
