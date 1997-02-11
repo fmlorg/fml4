f520392ec0c7efa623976bbd731a9edb6cb3c8f8
fukachan <fukachan>
1997-02-11 17:33:46 +0000

    Initial revision

---
+++ bin/emumail.pl
@@ -0,0 +1,64 @@
+#!/usr/local/bin/perl
+# Copyright (C) 1993-1996 fukachan@phys.titech.ac.jp
+# Copyright (C) 1996-1997 fukachan@sapporo.iij.ad.jp
+# fml is free software distributed under the terms of the GNU General
+# Public License. see the file COPYING for more details.
+
+&Init;
+&GetTime;
+$MessageId = "<$CurrentTime/$$.$USER\@$DOMAINNAME>";
+print &EmuHeader;
+exit 0;
+
+
+sub GetTime
+{
+    @WDay = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+    @Month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+	      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+    
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+    $Now = sprintf("%2d/%02d/%02d %02d:%02d:%02d", $year, $mon + 1, 
+		   $mday, $hour, $min, $sec);
+    $MailDate = sprintf("%s, %d %s %d %02d:%02d:%02d %s", $WDay[$wday],
+			$mday, $Month[$mon], $year, $hour, $min, $sec, $TZone);
+
+    # /usr/src/sendmail/src/envelop.c
+    #     (void) sprintf(tbuf, "%04d%02d%02d%02d%02d", tm->tm_year + 1900,
+    #                     tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min);
+    # 
+    $CurrentTime = sprintf("%04d%02d%02d%02d%02d", 1900 + $year, 
+			   $mon + 1, $mday, $hour, $min);
+}
+
+sub EmuHeader
+{
+qq#From $USER\@$DOMAINNAME $MailDate
+Date: $MailDate
+From: $USER\@$DOMAINNAME
+Message-Id: $MessageId
+To: (list suppressed)
+Subject: test mail
+
+#;
+}
+
+sub Init
+{
+    $USER = $ENV{'USER'} || (getpwuid($<))[0];
+
+    # DNS AutoConfigure to set FQDN and DOMAINNAME; 
+    local(@n, $hostname, $list);
+    chop($hostname = `hostname`); # beth or beth.domain may be possible
+    $FQDN = $hostname;
+    @n    = (gethostbyname($hostname))[0,1]; $list .= " @n ";
+    @n    = split(/\./, $hostname); $hostname = $n[0]; # beth.dom -> beth
+    @n    = (gethostbyname($hostname))[0,1]; $list .= " @n ";
+
+    foreach (split(/\s+/, $list)) { /^$hostname\.\w+/ && ($FQDN = $_);}
+    $FQDN       =~ s/\.$//; # for e.g. NWS3865
+    $DOMAINNAME = $FQDN;
+    $DOMAINNAME =~ s/^$hostname\.//;
+}
+
+1;
---
+++ proc/libmodeutils.pl
@@ -0,0 +1,82 @@
+# Copyright (C) 1993-1996 fukachan@phys.titech.ac.jp
+# Copyright (C) 1996-1997 fukachan@sapporo.iij.ad.jp
+# fml is free software distributed under the terms of the GNU General
+# Public License. see the file COPYING for more details.
+
+local($id);
+$id = q$Id$;
+$rcsid .= " :".($id =~ /Id: lib(.*).pl,v\s+(\S+)\s+/ && $1."[$2]");
+
+sub SubMode
+{
+    local($mode) = @_;
+
+    &StdinLogMode                     if $mode eq 'stdinlog';
+    &SpeculateContorolOrDistriuteMode if $mode eq 'distorctl';
+    &AppendMimeDecodedSubjectMode     if $mode eq 'mimedecodedsubject';
+}
+
+
+# THIS IS NOT 100% OK, ONLY SPECULATEION.
+sub SpeculateContorolOrDistriuteMode
+{
+    $START_HOOK .= q#;
+    local($ca) = &CutFQDN($CONTROL_ADDRESS);
+    if (($ca && ($Envelope{'mode:chk'} =~ /$ca/i))) {
+	$LOAD_LIBRARY || ($LOAD_LIBRARY = 'libfml.pl'); 
+        $COMMAND_ONLY_MODE = 1; 
+    }
+    else {
+        &DEFINE_MODE('distribute');
+    }
+    #;
+}
+
+sub StdinLogMode
+{
+    $START_HOOK .= q#;
+    &use('debug');
+    &StdinLog;
+    #;
+}
+
+
+sub AppendMimeDecodedSubjectMode
+{
+    $START_HOOK .= q# &AppendMimeDecodedSubject(*Envelope);#;
+
+}
+
+sub AppendMimeDecodedSubject
+{
+    local(*e) = @_;
+    local($append, $s, $jin);
+
+    $s   = $e{"h:Subject:"};
+    $jin = '\033\$[\@B]';
+
+    # if MIME and Japanese mixed?
+    if ($s =~ /$jin/i) {
+	$e{'h:X-Subject:'} = $s;
+	$e{'h:Subject:'} = &mimedecode($s);
+	$append++;
+    }
+
+    # IF Subject: MIME, Append  X-Subject: MIME-Decoded
+    if ($s =~ /ISO\-2022\-JP/i) {
+	&use('MIME');
+	$e{'h:X-Subject:'} = &mimedecode($s);
+	$append++;
+    }
+
+    if ($append) {
+	local(@h);
+	for (@HdrFieldsOrder) {
+	    push(@h, $_);
+	    if ('Subject' eq $_) { push(@h, 'X-Subject');}
+	}
+	@HdrFieldsOrder = @h;
+    }
+}
+
+1;
