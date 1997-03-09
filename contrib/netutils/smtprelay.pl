524b0bd38181c53130a136c514faa8ac02522d45
fukachan <fukachan>
1997-03-09 11:38:43 +0000

    *** empty log message ***

--- contrib/netutils/smtprelay.pl
+++ contrib/netutils/smtprelay.pl
@@ -1,4 +1,4 @@
-# Smtp Relay usling libsmtp.pl
+#!/usr/local/bin/perl
 # Copyright (C) 1996-1997 fukachan@sapporo.iij.ad.jp
 # fml is free software distributed under the terms of the GNU General
 # Public License. see the file COPYING for more details.
