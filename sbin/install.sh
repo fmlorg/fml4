3c4aae40d47cbcb99656712a7f48e77c1e341870
fukachan <fukachan>
1997-05-17 06:43:13 +0000

    cp -> tar link is done here (when make install)

--- sbin/install.sh
+++ sbin/install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+list=/tmp/.fml-install$$
+
 eval umask 022;
+
+trap "rm -f $list" 0 1 2 3 15
 
 DIRS="bin sbin libexec cf etc doc doc/html"
 EXEC_DIR=$1
 ARCH_DIR="$1/arch"
 DOC_DIR="$EXEC_DIR/doc"
-DRAFTS_DIR="$DOC_DIR/drafts"
+DRAFTS_DIR="$EXEC_DIR/drafts"
 
 test -d $EXEC_DIR   || mkdir $EXEC_DIR
 test -d $ARCH_DIR   || mkdir $ARCH_DIR
@@ -17,7 +21,11 @@ for dir in $DIRS
 do
 	echo "Installing $dir ..."
 	rm -fr $EXEC_DIR/$dir
-	tar cf - $dir|(cd $EXEC_DIR; tar xf -)
+	find $dir -type f -print |grep -v '\.bak' > $list
+
+	echo "----"; echo $dir; cat $list; echo "--------";
+
+	tar cf - `cat $list` | (cd $EXEC_DIR; tar xf - )
 done
 
 echo "Installing perl scripts (fml-source/src/*.pl) files ..."
@@ -27,10 +35,47 @@ chmod -R +w $EXEC_DIR/*
 test -d $DOC_DIR    || mkdir $DOC_DIR
 test -d $DRAFTS_DIR || mkdir $DRAFTS_DIR
 
-cp -p fml.c src/*.pl $EXEC_DIR
-cp -p src/arch/*.pl $ARCH_DIR
-cp -p doc/drafts/* $DRAFTS_DIR
-cp -p sbin/makefml $EXEC_DIR/makefml
+(cd src; tar cf - *.pl )      | (cd $EXEC_DIR; tar xf - )
+(cd src/arch/; tar cf - *.pl) | (cd $ARCH_DIR; tar xf - )
+(cd doc/drafts/; tar cf - .)  | (cd $DRAFTS_DIR; tar xf - )
+
+(
+	cd $EXEC_DIR;
+
+	# link doc/drafts ?
+	# (chdir doc/; ln -s ../drafts .)
+
+	rm -f libexec/listserv_compat.pl
+	ln libexec/fmlserv.pl libexec/listserv_compat.pl
+
+	rm -f libexec/majordomo_compat.pl
+	ln libexec/fmlserv.pl libexec/majordomo_compat.pl
+
+	rm -f libexec/fml_local2.pl 
+	ln libexec/fml_local.pl libexec/fml_local2.pl 
+
+	rm -f bin/fml_local.pl 
+	ln libexec/fml_local.pl bin/fml_local.pl 
+
+	rm -f bin/pop2recv.pl
+	ln libexec/popfml.pl bin/pop2recv.pl
+
+	rm -f bin/localtest.pl
+	ln sbin/localtest.pl bin/localtest.pl
+
+	rm -f bin/inc_via_pop.pl
+	ln bin/pop2recv.pl bin/inc_via_pop.pl
+
+	rm -f doc/fml_operations_manual
+	ln -s doc/op doc/fml_operations_manual
+
+	rm -f "doc/Daemon_Book_of_fml"
+	ln -s doc/op "doc/Daemon_Book_of_fml"
+)
+
+
+cp C/fml.c $EXEC_DIR
+cp -p sbin/makefml  $EXEC_DIR/makefml
 
 chmod 755 $EXEC_DIR/fml.pl $EXEC_DIR/msend.pl $EXEC_DIR/makefml 
 chmod 755 $EXEC_DIR/libexec/* $EXEC_DIR/bin/* $EXEC_DIR/sbin/*
