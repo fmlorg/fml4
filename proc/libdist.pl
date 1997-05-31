d5e4268b2847c62cabad12864dac4294fe82f816
fukachan <fukachan>
1997-05-31 13:24:46 +0000

    *** empty log message ***

--- proc/libdist.pl
+++ proc/libdist.pl
@@ -2,7 +2,7 @@
 # Copyright (C) 1996-1997 fukachan@sapporo.iij.ad.jp
 # fml is free software distributed under the terms of the GNU General
 # Public License. see the file COPYING for more details.
-
+#
 # $Id$;
 
 
