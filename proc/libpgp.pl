7b5b123383837d01d3d051c5bcd4122f96064b67
fukachan <fukachan>
1997-07-19 03:35:37 +0000

    a log of fixes

--- proc/libpgp.pl
+++ proc/libpgp.pl
@@ -10,28 +10,39 @@ sub PGPGoodSignatureP
     local(*e) = @_;
     local($auth) = 0;
 
-    if ($e{'Body'} = ~ /^[\s\n]*$/) {
+    &Log("PGPGoodSignatureP") if $debug;
+
+    if ($e{'Body'} =~  /^[\s\n]*$/) {
 	&Log("PGPGoodSignatureP Error: no effective mailbody");
-	return $auth;
-    }
-    if (! -x $PGP) {
-	&Log("PGPGoodSignatureP Error: \$PGP NOT DEFINED");
+	&Mesg(*e, "Mail Body has no PGP Signature");
 	return $auth;
     }
 
-    require 'open2.pl';
+    # program exeistence check
+    if (! -x $PGP) {
+	&Log("PGPGoodSignatureP Error: program \$PGP is NOT DEFINED");
+	&Mesg(*e, "PGP Environment Error");
+	return $auth;
+    }
+
+    # English mode is required
+    local($pgp_opts) = "+Language=en";
 
     # 2>&1 is required to detect "Good signature"
-    &open2(RPGP, WPGP, "$PGP -f 2>&1") || die("$!");
+    require 'open2.pl';
+    &open2(RPGP, WPGP, "$PGP $pgp_opts -f 2>&1") || &Log("PGP: $!");
     print WPGP "\n";
     print WPGP $e{'Body'};
     print WPGP "\n";
     close(WPGP);
 
     while (<RPGP>) {
-	$auth = 1 if /Good\s+signature/;
+	$auth = 1 if /Good\s+signature/i;
 	print STDERR "PGP OUT:$_" if $debug;
     }
+
+    # PGP authenticated
+    &Mesg(*e, $auth ? "PGP: Good signature." : "PGP: No good signature.");
 
     $auth;
 }
