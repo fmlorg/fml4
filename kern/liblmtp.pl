b0cefcfa21c8d72ab8959efb82ce9540f7d34ccf
fukachan <fukachan>
1997-10-30 11:58:31 +0000

    LMTP initial submit

---
+++ kern/liblmtp.pl
@@ -0,0 +1,49 @@
+# Copyright (C) 1993-1997 Ken'ichi Fukamachi
+#          All rights reserved. 
+#               1993-1996 fukachan@phys.titech.ac.jp
+#               1996-1997 fukachan@sapporo.iij.ad.jp
+# 
+# FML is free software; you can redistribute it and/or modify
+# it under the terms of GNU General Public License.
+# See the file COPYING for more details.
+#
+# $Id$
+#
+
+sub DoSmtpFeed
+{
+    ;
+}
+
+
+sub SetupSmtpFeed
+{
+    $smtpfeed = &SearchPath("smtpfeed", "/usr/libexec") || 
+	"/usr/libexed/smtpfeed";
+
+    require 'open2.pl';
+    if (&open2(RS, S, $smtpfeed)) { 
+	&Log("open2(RS, S, $smtpfeed)") if $debug;
+    }
+    else {
+	&Log("SmtpIO: cannot exec $smtpfeed");
+	return "SmtpIO: cannot exec $smtpfeed";
+    };
+}
+
+
+package lmtp;
+
+sub Copy
+{
+    local($in, $out) = @_;
+    open(IN,  $in)      || (&Log("CopyIN: $!"), return);
+    open(OUT, "> $out") || (&Log("CopyOUT: $!"), return);
+    select(OUT); $| = 1; select(STDOUT); 
+    while (<IN>) { print OUT $_;}
+    close(OUT);
+    close(IN); 
+}
+
+
+1;
