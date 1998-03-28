1db076cfbde189200ccb10b88e484a4f874fb92d
fukachan <fukachan>
1998-03-28 12:24:27 +0000

    update copyright

---
+++ etc/makefml/include-ctl.cmd
@@ -0,0 +1,1 @@
+_PERL_PATH_ _EXEC_DIR_\wrapper.pl -p _PERL_PATH_ _EXEC_DIR_\fml.pl _ML_DIR_\_ML_ _EXEC_DIR_ _OPTIONS_ --ctladdr
---
+++ etc/makefml/msend.cmd
@@ -0,0 +1,1 @@
+_PERL_PATH_ _EXEC_DIR_\wrapper.pl -p _PERL_PATH_ _EXEC_DIR_\msend.pl _ML_DIR_\_ML_ _EXEC_DIR_ _OPTIONS_
