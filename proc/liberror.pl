1be69ab14beb5c989ad4ad0a1dbdf74e0f12ee28
fukachan <fukachan>
1998-09-06 04:58:16 +0000

    new track

--- proc/liberror.pl
+++ proc/liberror.pl
@@ -1,21 +1,89 @@
+# Copyright (C) 1993-1998 Ken'ichi Fukamachi
+#          All rights reserved. 
+#               1993-1996 fukachan@phys.titech.ac.jp
+#               1996-1998 fukachan@sapporo.iij.ad.jp
+# 
+# FML is free software; you can redistribute it and/or modify
+# it under the terms of GNU General Public License.
+# See the file COPYING for more details.
+#
+# $Id$
+#
+
+
 sub NotifyMailSizeOverFlow
 {
     local(*e) = @_;
     local($subj, $body);
 
-    &Log("Error: in-coming mail body size > $INCOMING_MAIL_SIZE_LIMIT");
-    &Log("Forward to \$MAINTAINER");
+    &Log("Error: in-coming mail body size > $INCOMING_MAIL_SIZE_LIMIT bytes.");
 
     $subj = 
 	"Error: In-coming mail body size > $INCOMING_MAIL_SIZE_LIMIT $ML_FN";
 	
     $body = "--- Error ---\n";
     $body .= 
-	"In-coming mail size exceeds $INCOMING_MAIL_SIZE_LIMIT bytes\n\n";
+	"In-coming mail size exceeds $INCOMING_MAIL_SIZE_LIMIT bytes.\n\n";
 
+    if ($NOTIFY_MAIL_SIZE_OVERFLOW) { 
+	$body .= "I'll warn it to the sender (since \$NOTIFY_MAIL_SIZE_OVERFLOW is set).\n";
+    }
+    if ($ANNOUNCE_MAIL_SIZE_OVERFLOW) {
+	$body .= "I'll announce it to ML if the content seems a posted article.\n";
+	$body .= "(since \$ANNOUNCE_MAIL_SIZE_OVERFLOW is set).\n";
+    }
+
+    $body .= "Here is the received mail.\n";
+    $body .= ("-" x 60). "\n";
+
+    # forwarded to maintainer
     $e{'ctl:smtp:stdin2socket'} = 1;
-    &Warn($subj, $body.$e{'Body'});
+    &Log("Forwarded to \$MAINTAINER");
+    &Warn($subj, $body.$e{'Header'}."\n".$e{'Body'});
     undef $e{'ctl:smtp:stdin2socket'};
+
+    if ($NOTIFY_MAIL_SIZE_OVERFLOW) { &NotifyMailSizeOver2Sender;}
 }
+
+
+sub NotifyMailSizeOver2Sender
+{
+    &Mesg(*e, "ATTENTION! Your mail is too big, so not processed!!!");
+    &Mesg(*e, "This ML <$MAIL_LIST> restricts the maximum mail size,");
+    &Mesg(*e, "so pay attention to the mail with e.g. attachments.");
+}
+
+
+sub AnnounceMailSizeOver
+{
+    local(*e) = @_;
+    local($h, $body);
+
+    $body  = "Hello, I am an FML mailing list (ML) server/driver.\n";
+    $body .= "This ML restricts the MAXIMUM MAIL SIZE.\n";
+    $body .= "I\'ve received the following mail and reject it\n";
+    $body .= "since it is ***** TOO BIG ***** !!!\n";
+    $body .= "\n";
+	
+    undef $e{'Body'};
+
+    # MIME decode
+    if ($h =~ /=\?ISO\-2022\-JP\?/io) {
+	$h = &DecodeMimeStrings($e{'Header'});
+    }
+    else {
+	$h = $e{'Header'};
+    }
+
+    $h =~ s/^/   /g;
+    $h =~ s/\n/\n   /g;
+    $e{'Body'} = "$body$h\n   ... (body is suppressed since too big) ...\n";
+
+    # reset header
+    $e{'h:Subject:'}  = "REJECT TOO BIG MAIL ($e{'h:Subject:'})";
+    $e{'h:From:'}     = $MAINTAINER;
+    $e{'h:Reply-To:'} = $MAIL_LIST;
+}
+
 
 1;
--- proc/libsmtpsd.pl
+++ proc/libsmtpsd.pl
@@ -1,3 +1,15 @@
+# Copyright (C) 1993-1998 Ken'ichi Fukamachi
+#          All rights reserved. 
+#               1993-1996 fukachan@phys.titech.ac.jp
+#               1996-1998 fukachan@sapporo.iij.ad.jp
+# 
+# FML is free software; you can redistribute it and/or modify
+# it under the terms of GNU General Public License.
+# See the file COPYING for more details.
+#
+# $Id$
+#
+
 package sd;
 
 sub Log 
@@ -12,10 +24,10 @@ sub main'SDInit #';
 
     $tmp_dir  = $main'FP_TMP_DIR; #';
     $dir      = $main'DIR; #';
-    $SD_TMP   = "$tmp_dir/sd$$";
-    $SD_CACHE = "$dir/sd.cache";
+    $TmpFile  = "$tmp_dir/sd$$";
+    $SDCache = "$dir/sd.cache";
 
-    if (open(OUTLIST, "| sort > $SD_TMP")) {
+    if (open(OUTLIST, "| sort > $TmpFile")) {
 	select(OUTLIST); $| = 1; select(STDOUT);
     }
     else {
@@ -44,7 +56,7 @@ sub main'SDInit #';
     close(OUTLIST);
 
     ### cache out
-    if (open(OUTLIST, "> $SD_CACHE")) {
+    if (open(OUTLIST, "> $SDCache")) {
 	select(OUTLIST); $| = 1; select(STDOUT);
     }
     else {
@@ -52,7 +64,7 @@ sub main'SDInit #';
 	return 0;
     }
 
-    open(LIST, $SD_TMP) || do { &Log("SDInit: cannot open $SD_TMP"); next;};
+    open(LIST, $TmpFile) || do { &Log("SDInit: cannot open $TmpFile"); next;};
     while (<LIST>) {
 	chop;
 	($domain, $_) = split(/\s+/, $_);
@@ -63,11 +75,11 @@ sub main'SDInit #';
     close(OUTLIST);
     close(LIST);
 
-    unlink $SD_TMP;
+    unlink $TmpFile;
 
     # save list
-    @ORG_LIST = @list;
-    @list = ($SD_CACHE);
+    @OrgList = @list;
+    @list = ($SDCache);
 }
 
 
@@ -75,8 +87,8 @@ sub main'SDFin #';
 {
     local(*list) = @_;
 
-    unlink $SD_CACHE;
-    @list = @ORG_LIST;
+    unlink $SDCache;
+    @list = @OrgList;
 }
 
 
