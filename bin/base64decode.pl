61c31273b063a79f632afd585b1964a5bd09bb21
fukachan <fukachan>
1998-11-01 15:16:34 +0000

    Initial revision

---
+++ bin/base64decode.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl
+#
+# Copyright (C) 1993-1998 Ken'ichi Fukamachi
+#          All rights reserved. 
+#               1993-1996 fukachan@phys.titech.ac.jp
+#               1996-1998 fukachan@sapporo.iij.ad.jp
+# 
+# FML is free software; you can redistribute it and/or modify
+# it under the terms of GNU General Public License.
+# See the file COPYING for more details.
+#
+# $Id$
+#
+
+require 'getopts.pl';
+&Getopts("dI:");
+
+$libdir = $0; 
+$libdir =~ s/base64decode.pl//;
+push(@INC, "$libdir/../");
+
+if ($opt_I) {
+    for (split(/:/, $opt_I)) { push(@INC, $_);}
+}
+
+require 'mimer20alpha.pl';
+
+undef $/;
+$body = <>;
+print &bodydecode($body);
+
+
+1;
