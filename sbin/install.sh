d34e50d478d30f849d43afb16b8abc15c42a33d0
fukachan <fukachan>
1998-11-05 03:27:03 +0000

    a big change: tar -> cp+mv style

--- sbin/install.sh
+++ sbin/install.sh
@@ -10,45 +10,100 @@
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 
+#####   functions   #####
+MOVE () {
+	suffix=new$$	
 
+	# echo "cp -p $src $dst.$suffix"
+	cp -p $src $dst.$suffix
+
+	if [ -x $src ] 
+	then
+		chmod 755 $dst.$suffix
+	fi
+
+	# echo "mv -f $dst.$suffix $dst"
+	mv -f $dst.$suffix $dst
+}
+#########################
+
+##### variables
 list=/tmp/.fml-install$$
 
 eval umask 022;
 
-trap "rm -f $list" 0 1 2 3 15
-
-DIRS="bin sbin libexec cf etc doc doc/html"
+DIRS="bin sbin libexec cf etc sys src drafts doc"
+CHDIRS="bin sbin libexec"
 EXEC_DIR=$1
-ARCH_DIR="$1/arch"
+SYS_DIR="$1/sys"
 DOC_DIR="$EXEC_DIR/doc"
 DRAFTS_DIR="$EXEC_DIR/drafts"
 
+trap "rm -f $list" 0 1 3 15
+
+##### MAIN #####
+
 test -d $EXEC_DIR   || mkdir $EXEC_DIR
-test -d $ARCH_DIR   || mkdir $ARCH_DIR
+test -d $SYS_DIR    || mkdir $SYS_DIR
 test -d $DOC_DIR    || mkdir $DOC_DIR
 test -d $DRAFTS_DIR || mkdir $DRAFTS_DIR
 
+
+### chmod ###
+
+for dir in $CHDIRS
+do
+	chmod 755 $dir/*
+done
+
+chmod 755 src/fml.pl src/msend.pl makefml
+
+
+### here we go! ###
 for dir in $DIRS
 do
 	echo "Installing $dir ..."
-	rm -fr $EXEC_DIR/$dir
-	find $dir -type f -print |grep -v '\.bak' > $list
 
-	# echo "----"; echo $dir; cat $list; echo "--------";
+	# making directories
+	find $dir -type d -print|while read x
+	do  
+		# "src" is an exceptional!!!
+		if [ X$x != Xsrc ]
+		then
+		   test -d $EXEC_DIR/$x || mkdir $EXEC_DIR/$x
+		fi
+	done
 
-	tar cf - `cat $list` | (cd $EXEC_DIR; tar xf - )
+	# check files to install
+	find $dir -type f -print |\
+	sed s#^$dir/## |grep -v '\.bak' > $list
+
+
+	cat $list | while read x
+	do
+	   src=$dir/$x
+	   dst=$EXEC_DIR/$dir/$x
+
+	   # "src" is an exceptional!!!
+	   if [ X$dir = Xsrc ]
+	   then
+		dst=$EXEC_DIR/$x
+	   else
+	   	test -d $EXEC_DIR/$dir || mkdir $EXEC_DIR/$dir
+	   fi
+
+	   MOVE
+	done
 done
 
-echo "Installing perl scripts (fml-source/src/*.pl) files ..."
 chmod -R +w $EXEC_DIR/*
 
-# since rm -fr ...
-test -d $DOC_DIR    || mkdir $DOC_DIR
-test -d $DRAFTS_DIR || mkdir $DRAFTS_DIR
+cp C/fml.c $EXEC_DIR
+cp -p sbin/makefml*  $EXEC_DIR
 
-(cd src; tar cf - *.pl )      | (cd $EXEC_DIR; tar xf - )
-(cd src/arch/; tar cf - .)    | (cd $ARCH_DIR; tar xf - )
-(cd doc/drafts/; tar cf - .)  | (cd $DRAFTS_DIR; tar xf - )
+chmod 755 $EXEC_DIR/fml.pl $EXEC_DIR/msend.pl $EXEC_DIR/makefml*
+chmod 755 $EXEC_DIR/libexec/* $EXEC_DIR/bin/* $EXEC_DIR/sbin/*
+
 
 (
 	cd $EXEC_DIR;
@@ -76,21 +131,7 @@ test -d $DRAFTS_DIR || mkdir $DRAFTS_DIR
 
 	rm -f bin/inc_via_pop.pl
 	ln bin/pop2recv.pl bin/inc_via_pop.pl
-
-	(cd doc;
-		rm -f doc/fml_operations_manual
-		ln -s op fml_operations_manual
-
-		rm -f "doc/Daemon_Book_of_fml"
-		ln -s op "Daemon_Book_of_fml"
-	)
 )
 
-
-cp C/fml.c $EXEC_DIR
-cp -p sbin/makefml*  $EXEC_DIR
-
-chmod 755 $EXEC_DIR/fml.pl $EXEC_DIR/msend.pl $EXEC_DIR/makefml*
-chmod 755 $EXEC_DIR/libexec/* $EXEC_DIR/bin/* $EXEC_DIR/sbin/*
 
 exit 0;
