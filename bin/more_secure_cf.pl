7caf848f3073a0b3f58698ec855ac21c91652fb1
fukachan <fukachan>
1998-12-26 15:29:07 +0000

    add FILTER_ATTR_REJECT_COMMAND

--- bin/more_secure_cf.pl
+++ bin/more_secure_cf.pl
@@ -13,10 +13,11 @@
 #
 
 %config = (
-	   'INCOMING_MAIL_SIZE_LIMIT', 512000,
-	   'USE_DISTRIBUTE_FILTER',    1,
-	   'USE_MTI',                  1,
-	   'USE_LOG_MAIL',             1,
+	   'INCOMING_MAIL_SIZE_LIMIT',   512000,
+	   'USE_DISTRIBUTE_FILTER',      1,
+	   'FILTER_ATTR_REJECT_COMMAND', 1,
+	   'USE_MTI',                    1,
+	   'USE_LOG_MAIL',               1,
 	   );
 
 
