080b37a489080904bd1696cbdfd34fb0d6b145db
fukachan <fukachan>
1999-01-10 10:33:57 +0000

    reconstruct doc generation

--- distrib/mk/fml.doc.mk
+++ distrib/mk/fml.doc.mk
@@ -1,24 +1,35 @@
 .include "distrib/mk/fml.prog.mk"
 
-# ordinary source
-DOC_RI_SRC     = README INSTALL INSTALL_on_NT4 INSTALL_with_QMAIL UPGRADE
-
-# these files need a special converter.
-DOC_RI_CNV_SRC = 
-
-### executables
-# document generators
-
+### executables; document generators ###
 # release snapshot generator library
-GEN_PLAIN_DOC = $(SH) $(DIST_DOC_BIN)/genplaindoc.sh
-
+GEN_PLAIN_DOC   = $(SH) $(DIST_DOC_BIN)/genplaindoc.sh
+DOC_GENERATOR   = $(SH) $(FML)/distrib/doc/bin/generator.sh
 DOC_GENERATOR_D = $(SH) $(FML)/distrib/doc/bin/depend_wrapper.sh
 DOC_CONV        = $(DOC_GENERATOR)
+FIX_WIX         = $(PERL) ${FML}/distrib/bin/fix-wix.pl -X ${FML}
+
+### sources ###
+HTML_MISC_SOURCES = roadmap.html nt.html search-j.html search-e.html
+
+### targets ###
+HTML_MISC         = var/html/index.html var/html/index-e.html
+HTML_SMM          = var/html/op/index.html var/html/op-e/index.html
+
+## doc/ri
+DOC_RI_SOURCES  = CHANGES CHECK_LIST FILES 
+DOC_RI_SOURCES += INSTALL INSTALL_on_NT4 INSTALL_with_QMAIL 
+DOC_RI_SOURCES += PORTINGS README UPGRADE
+DOC_RI_EXCEPTIONAL_SOURCES = RELEASE_NOTES INFO
 
 
+## doc/master
+DOC_DRAFT_SOURCES  = confirm confirmd.ackreq deny guide 
+DOC_DRAFT_SOURCES += help-admin help-fmlserv help objective welcome
 
 
-op: var/doc/op.jp
+### special PLAINDOC rurles (depends on *SOURCES*) ###
+.include "distrib/mk/fml.plaindoc.mk"
 
-var/doc/op.jp: doc/smm/*wix
-	env FML=${FML} $(SH) distrib/bin/DocReconfigure.op
+
+### special HTML rurles (depends on *SOURCES*) ###
+.include "distrib/mk/fml.htmldoc.mk"
