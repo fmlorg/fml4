104e5d4efa8f57838c8deba41eb739f77e907144
fukachan <fukachan>
1999-01-10 12:35:46 +0000

    define plain doc generator rurles

--- distrib/mk/fml.plaindoc.mk
+++ distrib/mk/fml.plaindoc.mk
@@ -2,3 +2,49 @@ op: var/doc/op.jp
 
 var/doc/op.jp: doc/smm/*wix
 	env FML=${FML} $(SH) distrib/bin/DocReconfigure.op
+
+
+.for file in ${DOC_DRAFT_SOURCES}
+__DOC_TARGETS__ += var/doc/${file}.jp
+__DOC_TARGETS__ += var/doc/${file}.en
+var/doc/${file}.jp: doc/drafts/${file}.wix
+	${FWIX} -n i doc/drafts/${file}.wix > var/doc/${file}.jp
+
+var/doc/${file}.en: doc/drafts/${file}.wix
+	${FWIX} -L ENGLISH -n i doc/drafts/${file}.wix > var/doc/${file}.en
+.endfor
+
+
+.for file in ${DOC_DRAFT_SOURCES}
+__DOC_TARGETS__ += var/doc/drafts/${file}.jp
+__DOC_TARGETS__ += var/doc/drafts/${file}.en
+var/doc/drafts/${file}.jp: doc/drafts/${file}.wix
+	${FWIX} -n i doc/drafts/${file}.wix > var/doc/drafts/${file}.jp
+
+var/doc/drafts/${file}.en: doc/drafts/${file}.wix
+	${FWIX} -L ENGLISH -n i doc/drafts/${file}.wix > var/doc/drafts/${file}.en
+.endfor
+
+
+
+.for file in ${DOC_RI_SOURCES}
+__DOC_TARGETS__ += var/doc/${file}.jp
+__DOC_TARGETS__ += var/doc/${file}.en
+var/doc/${file}.jp: doc/drafts/${file}.wix
+	${FWIX} -n i doc/drafts/${file}.wix > var/doc/${file}.jp
+
+var/doc/${file}.en: doc/drafts/${file}.wix
+	${FWIX} -L ENGLISH -n i doc/drafts/${file}.wix > var/doc/${file}.en
+.endfor
+
+
+### MAIN ###
+__initplaindocbuild__:
+	@ echo --plaindoc
+	@ test -d var/doc/drafts || mkdir var/doc/drafts
+	@ test -d var/doc || mkdir var/doc
+	@ echo Generating ...
+	@ echo ${__DOC_TARGETS__} | tr ' '  '\012'
+
+plaindocbuild: __initplaindocbuild__ ${__DOC_TARGETS__}
+	@ echo --plaindoc done.
