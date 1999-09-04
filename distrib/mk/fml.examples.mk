120c51a103d8cbcdceee17483b239eb2df42f88b
fukachan <fukachan>
1999-09-04 02:44:57 +0000

    restruct fml.examples.mk and add examples/ptr-*

--- distrib/mk/fml.examples.mk
+++ distrib/mk/fml.examples.mk
@@ -1,4 +1,8 @@
+### procs
 HTML_FILTER = $(FML)/bin/fwix.pl -m htmlconv 
+
+
+### fundamental rules
 
 .for file in ${DOC_EXAMPLE_SOURCES}
 __HTML_EXAMPLES__ += ${WORK_EXAMPLES_DIR}/${file}.html
@@ -13,21 +17,24 @@ ${WORK_EXAMPLES_DIR}/${file}-e.html: doc/examples/${file}.wix
 		-o ${WORK_EXAMPLES_DIR}/${file}-e.html doc/examples/${file}.wix
 .endfor
 
-# doc/examples/index{,-e}.html
-__HTML_EXAMPLES__ += ${WORK_EXAMPLES_DIR}/index.html
-__HTML_EXAMPLES__ += ${WORK_EXAMPLES_DIR}/index-e.html
 
-# doc/examples/*txt
+
+### doc/examples/*txt
 __HTML_EXAMPLES_TXT__ = makefml.cgi cgi-INSTALL cgi-TODO cgi-IMPLEMENTATION
-
-${WORK_EXAMPLES_DIR}/index.html: doc/examples/index.html
-	${JCONV} doc/examples/index.html > ${WORK_EXAMPLES_DIR}/index.html
-
-${WORK_EXAMPLES_DIR}/index-e.html: doc/examples/index-e.html
-	${JCONV} doc/examples/index-e.html > ${WORK_EXAMPLES_DIR}/index-e.html
 
 .for file in ${__HTML_EXAMPLES_TXT__}
 __HTML_EXAMPLES__ += ${WORK_EXAMPLES_DIR}/${file}.txt
 ${WORK_EXAMPLES_DIR}/${file}.txt: doc/examples/${file}.txt
 	${JCONV} doc/examples/${file}.txt > ${WORK_EXAMPLES_DIR}/${file}.txt
+.endfor
+
+
+### doc/examples/index{,-e}.html and doc/examples/*html
+__HTML_EXAMPLES_HTML__ += index index-e
+__HTML_EXAMPLES_HTML__ += ptr-customize-header
+
+.for file in ${__HTML_EXAMPLES_HTML__}
+__HTML_EXAMPLES__ += ${WORK_EXAMPLES_DIR}/${file}.html
+${WORK_EXAMPLES_DIR}/${file}.html: doc/examples/${file}.html
+	${JCONV} doc/examples/${file}.html > ${WORK_EXAMPLES_DIR}/${file}.html
 .endfor
