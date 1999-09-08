7f31d316219c9a2e3a8111f12534177952b296bc
fukachan <fukachan>
1999-09-08 11:47:22 +0000

    trap and rm -f temporary makefile

--- distrib/doc/bin/depend_wrapper.sh
+++ distrib/doc/bin/depend_wrapper.sh
@@ -3,11 +3,17 @@
 FILE=$1
 SOURCE_DIR=$2
 TARGET=${FILE}
-m=/tmp/_makefile
+
+# temporary Makefile
+m=/tmp/_makefile$$
+
+trap "rm -f $m" 0 1 3 15
 
 rm -f $m
 echo "DOC_CONV = /bin/sh \$(FML)/distrib/doc/bin/generator.sh" >> $m
 echo "var/html/$TARGET/index.html: doc/$SOURCE_DIR/$FILE.wix"        >> $m
 echo "	      (FML=$FML; export FML; cd doc/html; \$(DOC_CONV) $FILE $SOURCE_DIR)"  >> $m
 cd $FML
-exec make -f /tmp/_makefile
+make -f /tmp/_makefile
+
+exit 0
