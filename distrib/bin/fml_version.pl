3cac626aae4a996b73fa36025d12b2928d27721e
fukachan <fukachan>
2000-01-03 15:26:28 +0000

    YYYYMMDD returns YYYYMMDD.HHMM in gm zone

--- distrib/bin/fml_version.pl
+++ distrib/bin/fml_version.pl
@@ -187,7 +187,9 @@ sub GetTime
 sub YYYYMMDD
 {
     local($sec,$min,$hour,$mday,$mon,$year,$wday) = gmtime(time);
-    sprintf("%4d%02d%02d", $year + 1900, $mon + 1, $mday);
+    sprintf("%4d%02d%02d.%02d%02d",
+		$year + 1900, $mon + 1, $mday,
+		$hour, 0);
 }
 
 
