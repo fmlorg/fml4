9449f950d637ef22590dc16d70fb96b43d8e0e94
fukachan <fukachan>
2000-01-22 05:23:28 +0000

    Usage: bin/resend.pl [-dh] [ID] [RCPT]
        [options]
    
        -d debug
        -h this help
    
        ID     article number/ID
        RCPT   Receiver Email Address

---
+++ bin/resend.pl
@@ -0,0 +1,107 @@
+#!/usr/local/bin/perl
+#-*- perl -*-
+#
+# Copyright (C) 2000 Ken'ichi Fukamachi
+#          All rights reserved. 
+#
+# $Id$
+#
+
+# getopt()
+require 'getopts.pl';
+&Getopts("dhv");
+
+$opt_h && &Usage;
+
+$debug = $opt_d ? 1 : 0;
+
+eval($uname = `uname`);
+$uname || die("This program runs only on unix.\n");
+
+if (-f "config.ph") { require 'config.ph';}
+
+&Resend(@ARGV);
+
+exit 0;
+
+sub Usage
+{
+    $_ = qq#Usage: $0 [-dh] [ID] [RCPT];
+
+    [options];
+
+    -d debug;
+    -h this help;
+
+    ID     article number/ID;
+    RCPT   Receiver Email Address
+    #;
+
+    s/;//g;
+    print STDERR $_, "\n";
+    exit 0;
+}
+
+
+sub Resend
+{
+    local($id, $addr) = @_;
+    local($article);
+
+    &InitTTY;
+
+    while (! $id) {
+	print "Article ID: ";
+	$id = &GetString;
+	print "\n";
+    }
+
+    while (! $addr) {
+	print "address: ";
+	$addr = &GetString;
+	print "\n";
+    }
+
+    if (-f "$SPOOL_DIR/$id") {
+	$article = "$SPOOL_DIR/$id";
+    }
+
+    system "cat $article | sendmail $addr";
+}
+
+
+
+### Section: input
+
+sub InitTTY
+{
+    if (-e "/dev/tty") { $console = "/dev/tty";}
+
+    open(IN, "<$console") || open(IN,  "<&STDIN"); # so we don't dingle stdin
+    open(OUT,">$console") || open(OUT, ">&STDOUT");# so we don't dongle stdout
+    select(OUT); $| = 1; #select(STDOUT); $| = 1;
+}
+
+
+sub gets
+{
+    local($.);
+    $_ = <IN>;
+}
+
+
+sub GetString
+{
+    local($s);
+
+    $s = &gets;
+
+    # ^D
+    if ($s eq "")  { print STDERR "'^D' Trapped.\n"; exit 0;}
+    chop $s;
+
+    $s;
+}
+
+
+1;
