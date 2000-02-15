1ffaba36bf415a701ce004262fd8921e901facd1
fukachan <fukachan>
2000-02-15 11:31:27 +0000

    cut off rerere, Re(2) and Re*2 syntaxes

--- kern/fml.pl
+++ kern/fml.pl
@@ -895,7 +895,9 @@ sub CutOffRe
     # BBS style? CUT OFF 
     s/^\s*Re\d+:\s+/Re: /gi;     # Re2:
     s/^\s*Re\[\d+\]:\s+/Re: /gi; # Re[2]:
+    s/^\s*Re\(\d+\):\s+/Re: /gi; # Re(2):
     s/^\s*Re\^\d+:\s+/Re: /gi;   # Re^2:
+    s/^\s*Re\*\d+:\s+/Re: /gi;   # Re*2:
 
     while (s/^\s*Re:\s*Re:\s*/Re: /gi) { ;} #'/gi' for RE: Re: re: ;
     s/^\s*Re:\s+/Re: /; # canonicalize it to "Re: ";
