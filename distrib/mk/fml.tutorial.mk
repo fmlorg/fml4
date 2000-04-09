1882f6321bb0f398729fba0e784f0e74846d16f3
fukachan <fukachan>
2000-04-09 09:36:16 +0000

    define and enable html creation

---
+++ distrib/mk/fml.tutorial.mk
@@ -0,0 +1,28 @@
+HTML_FILTER = $(FML)/bin/fwix.pl -m html
+
+WORK_TUTORIAL_DIR = ${WORK_HTML_DIR}/${TUTORIAL_LANGUAGE}
+
+
+.for dir in ${DOC_TUTORIAL_SUBDIR}
+# prepare directory to be created
+__HTML_TUTORIAL__ += ${WORK_TUTORIAL_DIR}/${dir}
+__HTML_TUTORIAL__ += ${WORK_TUTORIAL_DIR}/${dir}/index.html
+
+# make directory
+${WORK_TUTORIAL_DIR}/${dir}:
+	test -d ${WORK_TUTORIAL_DIR}/${dir} ||\
+	   ${MKDIR} ${WORK_TUTORIAL_DIR}/${dir}
+
+# creation rule
+${WORK_TUTORIAL_DIR}/${dir}/index.html: doc/${TUTORIAL_LANGUAGE}/${dir}/*wix
+	${HTML_FILTER} -L ${TUTORIAL_LANGUAGE} \
+		-D ${WORK_TUTORIAL_DIR}/${dir} \
+		doc/${TUTORIAL_LANGUAGE}/${dir}/index.wix
+.endfor
+
+# make $LANGUAGE/tutorial.html
+__HTML_TUTORIAL__ += ${WORK_TUTORIAL_DIR}/tutorial.html
+${WORK_TUTORIAL_DIR}/tutorial.html: doc/${TUTORIAL_LANGUAGE}/INDEX
+	${PERL} distrib/bin/mkindex.pl \
+		-f doc/${TUTORIAL_LANGUAGE}/INDEX \
+		> ${WORK_TUTORIAL_DIR}/tutorial.html
