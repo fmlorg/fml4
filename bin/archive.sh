439f99e7f187113361d21efcdf1b599cee149c87
fukachan <fukachan>
2000-06-30 02:29:58 +0000

    clean up bin/archive.sh to enable to execute archive.pl automatically over all ML's under /var/spool/ml

--- bin/archive.sh
+++ bin/archive.sh
@@ -9,7 +9,28 @@
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 
-(cd /var/spool/ml/Elena;    /usr/local/bin/perl bin/Archive.pl)
-(cd /var/spool/ml/Freekick; /usr/local/bin/perl bin/Archive.pl)
+
+CONFIG_DIR=`dirname $0`
+CONFIG_DIR="$CONFIG_DIR/../.fml"
+
+if [ -f $CONFIG_DIR/system.sh ];then
+	. $CONFIG_DIR/system.sh
+else
+	echo "ERROR: cannot find $CONFIG_DIR/system.sh"
+	exit 1
+fi
+
+
+chdir $ML_DIR || cannot_chdir $ML_DIR
+
+for ml in *
+do
+   if [ -d "$ML_DIR/$ml" -a -f "$ML_DIR/$ml/config.ph" ]
+   then
+	(cd $ML_DIR/$ml
+	   $EXEC_DIR/bin/archive.pl
+	)
+   fi
+done
 
 exit 0;
