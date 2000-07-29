273ad2093822bffc8dcf283666ac000b1060eef3
fukachan <fukachan>
2000-07-29 04:15:09 +0000

    check address syntax more severely

--- libexec/mead.pl
+++ libexec/mead.pl
@@ -809,24 +809,41 @@ sub SearchPath
 }
 
 
+sub ValidAddressP($addr)
+{
+    my ($a) = @_;
+    $a =~ /^[-a-z0-9\._]+\@[a-z0-9\.-]+$/i ? 1 : 0;
+}
+
 sub Action
 {
     local($addr, $ml) = @_;
+    my ($mode) = $MODE;
 
     ### makefml -> $LogBuf{$ml}
     ### report     $Template{$ml} (command template)
     ###            $MakeFmlTemplate{$ml} (e.g. "makefml bye $ml $addr");
 
-    if ($MODE eq 'auto') {
-	&MakeFml("$ACTION $ml $addr", $addr, $ml);
-    }
-    elsif ($MODE eq 'report') {
-	$Template{$ml}            .= $CTK{$ml}."admin $ACTION $addr\n";
-	$MakeFmlTemplate{$ml}     .= "$MAKEFML $ACTION $ml $addr\n";
-	$MakeFmlTemplateAddr{$ml} .= $addr. "\t";
+    if (&ValidAddressP($addr)) {
+	if ($mode eq 'auto') {
+	    &MakeFml("$ACTION $ml $addr", $addr, $ml);
+	}
+	elsif ($mode eq 'report') {
+	    $Template{$ml}            .= $CTK{$ml}."admin $ACTION $addr\n";
+	    $MakeFmlTemplate{$ml}     .= "$MAKEFML $ACTION $ml $addr\n";
+	    $MakeFmlTemplateAddr{$ml} .= $addr. "\t";
+	}
+	else {
+	    &Debug("mode $mode is unknown");
+	}
     }
     else {
-	&Debug("mode $MODE is unknown");
+	&Debug("ignore invalid address $addr");
+
+	$MFSummary{$ml} .= "\tignore invalid address <$addr>.\n";
+	$MFSummary{$ml} .= "\tPlease remove <$addr> by hand.\n";
+	$LogBuf{$ml} .= "\tignore invalid address <$addr>.\n";
+	$LogBuf{$ml} .= "\tPlease remove <$addr> by hand.\n";
     }
 
     # record the addr irrespective of succeess or fail
