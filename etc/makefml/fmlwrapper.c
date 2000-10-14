62aea0d536c5c6c1280f49644cc3cd74f4e09ea2
fukachan <fukachan>
2000-10-14 09:56:42 +0000

    use fmlwrapper.h not config.h

--- etc/makefml/fmlwrapper.c
+++ etc/makefml/fmlwrapper.c
@@ -1,8 +1,8 @@
 /*
-  Copyright (C) 1993-1998 Ken'ichi Fukamachi
+  Copyright (C) 1993-2000 Ken'ichi Fukamachi
            All rights reserved. 
                 1993-1996 fukachan@phys.titech.ac.jp
-                1996-1998 fukachan@sapporo.iij.ad.jp
+                1996-2000 fukachan@sapporo.iij.ad.jp
   
   FML is free software; you can redistribute it and/or modify
   it under the terms of GNU General Public License.
@@ -10,7 +10,7 @@
 */
 
 #include <stdio.h>
-#include "config.h"
+#include "fmlwrapper.h"
 
 static char rcsid[] = "$Id$";
 
