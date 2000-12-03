82adb323157746288900d0c90bdbb2c6670fd8f5
fukachan <fukachan>
2000-12-03 10:58:19 +0000

    add modifier tool to ensure documents jcode

---
+++ distrib/bin/fix_doc_jcode.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+nkf=`which nkf`
+
+tmp="/tmp/fml.buf.$$"
+trap "rm -f $tmp" 0 1 3 15
+
+# check
+if [ ! -x $nkf ];then
+	echo error: cannot find nkf
+	exit 1
+fi
+
+
+for x in INDEX *wix */*wix
+do
+	echo "euc: $x"
+	$nkf -e $x > $tmp; mv $tmp $x
+done
+
+
+for x in */*html
+do
+	echo "jis: $x"
+	$nkf -j $x > $tmp; mv $tmp $x 
+done
