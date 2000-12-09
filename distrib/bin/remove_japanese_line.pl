024b7a89e34e38a4665e009053975145702c328e
fukachan <fukachan>
2000-12-09 08:54:21 +0000

    -r (reverse mode) shows Japanese line only

--- distrib/bin/remove_japanese_line.pl
+++ distrib/bin/remove_japanese_line.pl
@@ -1,10 +1,28 @@
 #!/usr/local/bin/perl
+#-*- perl -*-
+#
+# Copyright (C) 2000 Ken'ichi Fukamachi
+#          All rights reserved. 
+#
+# $Id$
+# $NetBSD$
+# $FML$
+#
+
+# getopt()
+require 'getopts.pl';
+&Getopts("dhr");
 
 $re_euc_c  = '[\241-\376][\241-\376]';
 
 while (<>) {
-    next if /$re_euc_c/;
-    print;
+    if ($opt_r) {
+	if (/$re_euc_c/) { print;}
+    }
+    else {
+	next if /$re_euc_c/;
+	print;
+    }
 }
 
 exit 0;
