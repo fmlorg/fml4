e566311ed0103a5823bb7186caa44acfd93ea54a
fukachan <fukachan>
2000-12-11 13:59:01 +0000

    shorten copyright

--- bin/fwix.pl
+++ bin/fwix.pl
@@ -135,15 +135,6 @@ All rights of this page is reserved.
 # This Document(html format) is automatically geneareted by fwix.pl. 
 # fwix (Formatter of WIX Language) is fml document formatter system
 # designed to generate plaintext, html, texinfo and nroff from one file.
-#
-# Copyright (C) 1993-2000 Ken'ichi Fukamachi
-#          All rights reserved. 
-#               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-2000 fukachan@sapporo.iij.ad.jp
-# 
-# FML is free software; you can redistribute it and/or modify
-# it under the terms of GNU General Public License.
-# See the file COPYING for more details.
 </PRE>    
 %;
 }
