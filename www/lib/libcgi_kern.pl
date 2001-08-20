b32b09c3da5a1a86e2ab40414d4ec83d3ac21c6d
fukachan <fukachan>
2001-08-20 12:29:52 +0000

    fix copyright, add $FML$

--- www/lib/libcgi_kern.pl
+++ www/lib/libcgi_kern.pl
@@ -1,15 +1,13 @@
 #-*- perl -*-
 #
-# Copyright (C) 1993-2000 Ken'ichi Fukamachi
+# Copyright (C) 1999-2000 Ken'ichi Fukamachi
 #          All rights reserved. 
-#               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-2000 fukachan@sapporo.iij.ad.jp
 # 
 # FML is free software; you can redistribute it and/or modify
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $Id$
+# $FML$
 #
 
 
