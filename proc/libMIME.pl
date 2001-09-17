528b937de3c9e71d64e0e54c62b5d0d17d4dda66
fukachan <fukachan>
2001-09-17 15:17:42 +0000

    import libMIME.pl 2.19

--- proc/libMIME.pl
+++ proc/libMIME.pl
@@ -7,41 +7,61 @@
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# q$Id$;
+# $FML: libMIME.pl,v 2.19 2001/09/14 04:11:14 fukachan Exp $
+#
+
+use vars qw($debug 
+	    $MIME_BROKEN_ENCODING_FIXUP);
 
 require 'mimer.pl';
 
 
-sub ProbeMIMEpm
-{
-    ### PERL 5  
-    if ($] =~ /^5\./) {
-	if (! $HAS_MIME_BASE64_PM) {
-	    eval("use MIME::Base64;");
-	    $HAS_MIME_BASE64_PM =  ($@ eq '') ? 1 : -1;
-	}
-	if (! $HAS_MIME_QUOTED_PRINT_PM) {
-	    eval(" use MIME::QuotedPrint;");
-	    $HAS_QUOTED_PRINT_PM =  ($@ eq '') ? 1 : -1;
-	}
-    }    
-}
-
-
-sub DecodeMimeStrings { &DecodeMimeString(@_); }
-sub DecodeMimeString
+################################################################
+#
+# wrapper functions for fml internal use
+#
+sub EnvelopeMimeDecode
 { 
-    # &ProbeMiIMEpm;
+    local(*e) = @_;
 
-    # 2.1A4 test phase (2.1REL - 2.1A3 requires explicit $MIME_EXT_TEST=1)
-    if (1) {
-	&MIME'MimeDecode(@_);
-    }
-    else {
-	&mimedecode(@_);
-    }
+    # XXX malloc() too much?
+    $e{'Hdr'}  = &DecodeMimeString($e{'Hdr'});
+    $e{'Body'} = &DecodeMimeString($e{'Body'});
 }
 
+
+sub StripMIMESubject
+{
+    local(*e) = @_;
+
+    &Debug("MIME  INPUT:      [$_]") if $debug;
+    ($_ = $e{'h:Subject:'}) || return;
+    &Debug("MIME  INPUT GO:   [$_]") if $debug;
+    $_ = &DecodeMimeString($_);
+    &Debug("MIME  REWRITTEN 0:[$_]") if $debug;
+
+    # 97/03/28 trick based on fml-support:02372 (uematsu@iname.com)
+    $_ = &StripBracket($_);
+    $e{'h:Subject:'} = &EncodeMimeString("$_\n");
+    $e{'h:Subject:'} =~ s/\n$//;
+
+    &Debug("MIME OUTPUT:[$_]") if $debug;
+    &Debug("MIME OUTPUT:[". $e{'h:Subject:'}."]") if $debug;
+}
+
+
+
+################################################################
+#
+# MIME ENCODING
+#
+
+# XXX YOU SNOHLD NOT USE THESE FUNCTIONS IN fml main libraries
+# XXX defined just for convenience e.g. for old HOOKS
+sub MimeEncode        { &mimeencode(@_); }
+sub MIMEEncode        { &mimeencode(@_); }
+sub EncodeMimeString  { &mimeencode(@_); }
+sub EncodeMimeStrings { &mimeencode(@_); }
 
 sub mimeencode
 {
@@ -50,7 +70,7 @@ sub mimeencode
     my $pkg = 'IM::Iso2022jp';
     eval qq{ require $pkg; $pkg->import();};
     unless ($@) {
-	line_iso2022jp_mimefy($s);
+	line_iso2022jp_mimefy(&STR2JIS($s));
     }
     else {
 	Log("mimeencode cannot load $pkg, fallback to mimew.pl");
@@ -64,47 +84,21 @@ sub mimeencode
 }
 
 
+################################################################
+#
+# MIME DECODING
+#
 
-# defined for convenience
-sub MimeEncode { &mimeencode(@_); }
-sub MimeDecode { &DecodeMimeString(@_); }
-sub MIMEEncode { &mimeencode(@_); }
-sub MIMEDecode { &DecodeMimeString(@_); }
+# XXX YOU SNOHLD NOT USE THESE FUNCTIONS IN fml main libraries
+# XXX defined just for convenience e.g. for old HOOKS
+sub MimeDecode        { &DecodeMimeString(@_); }
+sub MIMEDecode        { &DecodeMimeString(@_); }
+sub DecodeMimeStrings { &DecodeMimeString(@_); }
 
-
-sub EnvelopeMimeDecode
+sub DecodeMimeString
 { 
-    local(*e) = @_;
-
-    if (1) {
-	# XXX malloc() too much?
-	$e{'Hdr'}  = &DecodeMimeStrings($e{'Hdr'});
-	$e{'Body'} = &DecodeMimeStrings($e{'Body'});
-    }
-    else {
-	$e{'Hdr'}  = &mimedecode($e{'Hdr'});
-	$e{'Body'} = &mimedecode($e{'Body'});
-    }
-}
-
-
-sub StripMIMESubject
-{
-    local(*e) = @_;
-
-    &Debug("MIME  INPUT:      [$_]") if $debug;
-    ($_ = $e{'h:Subject:'}) || return;
-    &Debug("MIME  INPUT GO:   [$_]") if $debug;
-    $_ = &mimedecode($_);
-    &Debug("MIME  REWRITTEN 0:[$_]") if $debug;
-
-    # 97/03/28 trick based on fml-support:02372 (uematsu@iname.com)
-    $_ = &StripBracket($_);
-    $e{'h:Subject:'} = &mimeencode("$_\n");
-    $e{'h:Subject:'} =~ s/\n$//;
-
-    &Debug("MIME OUTPUT:[$_]") if $debug;
-    &Debug("MIME OUTPUT:[". $e{'h:Subject:'}."]") if $debug;
+    # 2.1A4 test phase (2.1REL - 2.1A3 requires explicit $MIME_EXT_TEST=1)
+    &MIME::MimeDecode(@_);
 }
 
 
@@ -126,10 +120,10 @@ sub MimeDecode
 {
     local($_) = @_;
 
-    $MimeBEncPat = 
+    my $MimeBEncPat = 
 	'=\?[Ii][Ss][Oo]-2022-[Jj][Pp]\?[Bb]\?([A-Za-z0-9\+\/]+)=*\?=';
 
-    $MimeQEncPat = 
+    my $MimeQEncPat = 
 	'=\?[Ii][Ss][Oo]-2022-[Jj][Pp]\?[Qq]\?([\011\040-\176]+)=*\?=';
 
 
@@ -140,6 +134,11 @@ sub MimeDecode
     # XXX: 2.02
     s/$MimeBEncPat/&kconv(&base64decode($1))/geo;
     s/$MimeQEncPat/&kconv(&MimeQDecode($1))/geo;
+
+    if ($main::MIME_BROKEN_ENCODING_FIXUP) {
+	s/\0//g;
+	s/$/\x1b(B/;
+    }
 
     s/(\x1b[\$\(][BHJ@])+/$1/g;
 
@@ -154,18 +153,22 @@ sub MimeDecode
 
 package main;
 
-# debug #
+# for debug
 if ($0 eq __FILE__) {
+    eval q{ sub Log { print STDERR "LOG>", $_[0], "\n";} };
+
+    require 'libloadconfig.pl';
+
     while (<>) {
 	my ($x) = $_;
 	chop $x;
 
 	print "    IN> ", $x, "\n";
 
-	$x = &MimeEncode($x);
+	$x = &EncodeMimeString($x);
 	print "encode> ", $x, "\n";
 
-	$x = &MimeDecode($x);
+	$x = &DecodeMimeString($x);
 	print "decode> ", $x, "\n";
     }
 }
