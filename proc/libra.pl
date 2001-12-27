9bced2cc95291e77ecf5be4cd8d283e3e0fc3caf
fukachan <fukachan>
2001-12-27 05:15:40 +0000

    fix copyright

--- proc/libra.pl
+++ proc/libra.pl
@@ -1,13 +1,14 @@
-# Copyright (C) 1993-1999 Ken'ichi Fukamachi
+# Copyright (C) 1993-2001 Ken'ichi Fukamachi
 #          All rights reserved. 
 #               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-1999 fukachan@sapporo.iij.ad.jp
+#               1996-2001 fukachan@sapporo.iij.ad.jp
 # 
 # FML is free software; you can redistribute it and/or modify
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $Id$;
+# $FML$
+#
 
 # LOCAL SCOPE
 local(%AdminProcedure, $UnderAuth, $AttachRRF);
