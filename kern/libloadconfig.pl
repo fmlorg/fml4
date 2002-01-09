cfe771db35581fd8873d1213f1b5fe8771d565af
fukachan <fukachan>
2002-01-09 15:30:36 +0000

    sync w/ HEAD (2.21-2.22): fix DEFINE_FIELD_PAT_TO_REJECT() argument handling

--- kern/libloadconfig.pl
+++ kern/libloadconfig.pl
@@ -309,8 +309,11 @@ sub DEFINE_FIELD_OF_REPORT_MAIL
 
 sub DEFINE_FIELD_PAT_TO_REJECT
 { 
-    $REJECT_HDR_FIELD_REGEXP{$_[0]} = $_[1];
-    $REJECT_HDR_FIELD_REGEXP_REASON{$_[0]} = $_[2] if $_[2];
+    my ($key, $value) = @_;
+    $key =~ tr/A-Z/a-z/;
+    $key = &FieldCapitalize($key);
+    $REJECT_HDR_FIELD_REGEXP{ $key }        = $value;
+    $REJECT_HDR_FIELD_REGEXP_REASON{ $key } = $value if $value;
 }
 
 sub DEFINE_FIELD_LOOP_CHECKED
