12c3902d303ea068ebba4c61ab96cd7a4482d585
fukachan <fukachan>
2002-01-25 04:58:16 +0000

    sync w/ HEAD (2.141-2.142): fix wrong function call when $NOTIFY_MAIL_SIZE_OVERFLOW. applied patch by anthrax@unixuser.org, thanks.

--- kern/fml.pl
+++ kern/fml.pl
@@ -1,15 +1,15 @@
 #!/usr/local/bin/perl
 #
-# Copyright (C) 1993-2001 Ken'ichi Fukamachi
+# Copyright (C) 1993-2002 Ken'ichi Fukamachi
 #          All rights reserved. 
 #               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-2001 fukachan@sapporo.iij.ad.jp
+#               1996-2002 fukachan@sapporo.iij.ad.jp
 # 
 # FML is free software; you can redistribute it and/or modify
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $FML: fml.pl,v 2.124.2.6 2001/10/15 12:53:04 fukachan Exp $
+# $FML$
 
 $Rcsid   = 'fml 4.0';
 
@@ -1103,7 +1103,7 @@ sub CheckCurrentProc
 	    # WARNING in n ==1 partial case.
 	    if ($n == 1 && $NOTIFY_MAIL_SIZE_OVERFLOW) {
 		&use('error');
-		&NotifyMailSizeOver;
+                &NotifyMailSizeOverFlow(*e);
 	    } 
 
 	    if ($n == 1 && $ANNOUNCE_MAIL_SIZE_OVERFLOW) {
