8c597280c0c955a29ea71754c6e7cc73eda6ead0
fukachan <fukachan>
2003-05-01 14:26:29 +0000

    sync w/ HEAD (2.150-2.151): strict 2822 (PR fml-help:2364)

--- kern/fml.pl
+++ kern/fml.pl
@@ -9,7 +9,7 @@
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $FML$
+# $FML: fml.pl,v 2.124.2.15 2003/04/19 13:27:58 fukachan Exp $
 
 $Rcsid   = 'fml 4.0';
 
@@ -700,7 +700,7 @@ sub GetFieldsFromHeader
     if ($ALLOW_WRONG_LINES_IN_HEADER){
 	$s =~ s/(?<=\n)(?=:|[^\s:]+\s)/X-Wrong-Lines: /sg;
     }
-    $s =~ s/\n(\S+):/\n\n$1:\n\n/g; #  trick for folding and unfolding.
+    $s =~ s/\n(\S+?):/\n\n$1:\n\n/g; #  trick for folding and unfolding.
     $s =~ s/^\n*//;		# remove the first null lines;
 
     @hdr = split(/\n\n/, $s);
