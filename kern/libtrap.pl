94efaec3b691d8ad82f14edb0160d16ba9a89df6
fukachan <fukachan>
2003-12-13 01:40:08 +0000

    pull up 2.2-2.3

--- kern/libtrap.pl
+++ kern/libtrap.pl
@@ -1,15 +1,19 @@
 #-*- perl -*-
 #
-# Copyright (C) 1993-2000 Ken'ichi Fukamachi
+# Copyright (C) 1993-2001 Ken'ichi Fukamachi
 #          All rights reserved. 
 #               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-2000 fukachan@sapporo.iij.ad.jp
+#               1996-2001 fukachan@sapporo.iij.ad.jp
 # 
 # FML is free software; you can redistribute it and/or modify
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $Id$
+# $FML$
+#
+
+
+use vars qw($debug);
 
 
 ####### Section: TRAP
