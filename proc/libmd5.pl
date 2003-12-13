97e054b068d6f68fead6798bc0a331040a477250
fukachan <fukachan>
2003-12-13 06:02:40 +0000

    pull up 2.6-2.8

--- proc/libmd5.pl
+++ proc/libmd5.pl
@@ -1,25 +1,26 @@
-# Copyright (C) 1993-1998 Ken'ichi Fukamachi
+# Copyright (C) 1993-1999,2001 Ken'ichi Fukamachi
 #          All rights reserved. 
 #               1993-1996 fukachan@phys.titech.ac.jp
-#               1996-1998 fukachan@sapporo.iij.ad.jp
+#               1996-1999,2001 fukachan@sapporo.iij.ad.jp
 # 
 # FML is free software; you can redistribute it and/or modify
 # it under the terms of GNU General Public License.
 # See the file COPYING for more details.
 #
-# $Id$
+# $FML$
+#
 
-
+use vars qw($debug);
 use MD5;
 
 sub main::Md5
 {
-    local($data) = @_;
-    local($buf, $p, $pe);
+    my ($data) = @_;
+    my ($p, $pe);
 
     $pe = length($data);
 
-    $md5 = new MD5;
+    my $md5 = new MD5;
     $md5->reset();
 
     $p = 0;
@@ -38,11 +39,11 @@ sub main::Md5
 sub main::MailBodyMD5Cksum
 {
     local(*e) = @_;
-    local($buf, $p, $pe);
+    my ($p, $pe);
 
     $pe = length($e{'Body'});
 
-    $md5 = new MD5;
+    my $md5 = new MD5;
     $md5->reset();
 
     $p = 0;
